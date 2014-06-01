1ca0bf76d6a873026ff49ff72c4aaa653ae54eb9
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-06-01 07:30:16 +0000

    Added documentation

--- source/tessellation/polygon_overlap_area.hpp
+++ source/tessellation/polygon_overlap_area.hpp
@@ -12,11 +12,23 @@ class PolygonOverlap
 private:
   base_generator_type gen;
 public:
-
+//! \brief Class constructor
   PolygonOverlap(void);
 
+  /*!
+  \brief Calcualted the area of a convex polygon
+  \param polygon The vertices of the polygon, should be ordered as a convex hull
+  \return The area
+  */
   double PolyArea(vector<Vector2D> const& polygon);
-  
+  /*! 
+  \brief Calculates the area overlaped between two convex polygons
+  \param ch1 The convex hull of the first polygon
+  \param ch2 The convex hull of the second polygon
+  \param R0 The effective radius of the first polygon
+  \param R1 The effective radius of the second polygon
+  \return The overlaped area
+  */
   double polygon_overlap_area(vector<Vector2D> const& ch1,
 			      vector<Vector2D> const& ch2,double R0,double R1);
 };
