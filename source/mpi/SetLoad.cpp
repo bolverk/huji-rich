5140b550dd0bd986554fc845d04f0188dba3c27f
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-06-18 13:33:30 +0000

    Added function to improve the load balance before the simulation is started

---
+++ source/mpi/SetLoad.cpp
@@ -0,0 +1,31 @@
+#include "SetLoad.hpp"
+
+#ifdef RICH_MPI
+
+void SetLoad(Tessellation &tproc,vector<Vector2D> const& points,OuterBoundary const&
+	outer,int Nbest,int Niter)
+{
+	int rank,ws;
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&ws);
+	ConstNumberPerProc procmove(outer,Nbest,0.75);
+	VoronoiMesh local(points,tproc,outer);
+	for(int i=0;i<Niter;++i)
+	{
+		MPI_Barrier(MPI_COMM_WORLD);
+		procmove.Update(tproc,local);
+		vector<Vector2D> cp=local.GetMeshPoints();
+		cp.resize(local.GetPointNo());
+		MPI_Barrier(MPI_COMM_WORLD);
+		try
+		{
+			local.Update(cp,tproc);
+		}
+		catch(UniversalError const& eo)
+		{
+			DisplayError(eo);
+		}
+	}
+}
+
+#endif
