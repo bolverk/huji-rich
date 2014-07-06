b68a8a8fc1e65a40a1e58d4c1f3b11263a3c7c60
almog.yalin@gmail.com <almog.yalin@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-07-06 06:23:58 +0000

    Added documentation

--- source/mpi/ConstNumberPerProc.hpp
+++ source/mpi/ConstNumberPerProc.hpp
@@ -1,7 +1,12 @@
+/*! \file ConstNumberPerProc.hpp
+  \brief A class that tries to maintain a constant number of points per processor by solving eq 68 in AREPO's paper
+  \author Elad Steinberg
+ */
+
 #ifndef CONSTPERPROC
 #define CONSTPERPROC 1
 #include "ProcessorUpdate.hpp"
-//! \brief A class that tries to maintain a constant number of points per processor by solving eq 68 in AREPO's paper
+
 class ConstNumberPerProc: public ProcessorUpdate
 {
 public:
