bd07cd4256aa5688cdaa6d6393c3a753729d2ad2
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-07-27 08:33:14 +0000

    Fixed documentation

--- trunk/source/tessellation/EdgeLengthCorrect.hpp
+++ trunk/source/tessellation/EdgeLengthCorrect.hpp
@@ -17,7 +17,7 @@ void CorrectEdgeLength(Tessellation const& tessold,Tessellation const& tessnew,
 /*
 \brief Corrects the length of the edges to be second order in time
 \param tessold The tessellation at the start of the time step
-\param tessold The tessellation at the middle of the time step
+\param tessmid The tessellation at the middle of the time step
 \param tessnew The tessellation at the end of the time step
 \param lengths This is the output of the correct edges lengths
 */
