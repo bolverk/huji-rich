5a2401771d0a9f163da053fda25239d1ded37530
almog.yalin@gmail.com <almog.yalin@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-07-31 05:58:40 +0000

    Updated signature

--- trunk/source/newtonian/test_2d/RT.hpp
+++ trunk/source/newtonian/test_2d/RT.hpp
@@ -18,7 +18,7 @@ public:
 
   RT_velocityY();
   ~RT_velocityY();
-  double EvalAt(Vector2D const& r) const;
+  double operator()(Vector2D const& r) const;
 };
 
 //! \brief Spatial profile of the initial pressure for Rayleigh Taylor instability
@@ -38,7 +38,7 @@ public:
 
   ~RT_Pressure();
 
-  double EvalAt(Vector2D const& r) const;
+  double operator()(Vector2D const& r) const;
 };
 
 
