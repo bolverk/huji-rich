4ef70738e65745cb063cc32dc7bc488807555ace
almog.yalin <almog.yalin@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-08-16 10:49:54 +0000

    Updated example

--- trunk/examples/sedov_taylor_error.cpp
+++ trunk/examples/sedov_taylor_error.cpp
@@ -11,22 +11,42 @@
 #include "source/newtonian/two_dimensional/point_motions/round_cells.hpp"
 #include "source/misc/mesh_generator.hpp"
 #include "source/newtonian/two_dimensional/hdf5_diagnostics.hpp"
-#include "source/misc/int2str.hpp"
-#include "source/newtonian/two_dimensional/source_terms/CenterGravity.hpp"
 #include "source/tessellation/shape_2d.hpp"
 #include "source/newtonian/test_2d/piecewise.hpp"
+#include "source/newtonian/test_2d/main_loop_2d.hpp"
+#include "source/newtonian/test_2d/consecutive_snapshots.hpp"
+#include "source/newtonian/test_2d/multiple_diagnostics.hpp"
+
+class ProgressReport: public DiagnosticFunction
+{
+public:
+
+  ProgressReport(int skip):
+    skip_(skip) {}
+
+  void operator()(const hdsim& sim)
+  {
+    if(sim.GetCycle()%skip_==0)
+      cout << "Sim time is " << sim.GetTime() << " Step number "
+	   << sim.GetCycle() << endl;
+  }
+
+private:
+  const int skip_;
+};
 
 int main(void)
 {
-  // Set up the initial grid points
-  int npointsx=50;
-  int npointsy=50;
-  double widthx=2;
-  double widthy=2;
-  vector<Vector2D> InitPoints=SquareMesh(npointsx,npointsy,widthx,widthy);
-
   // Set up the boundary type for the points
-  SquareBox outer(-widthx/2,widthx/2,widthy/2,-widthy/2);
+  SquareBox outer(Vector2D(-0.5,-0.5),
+		  Vector2D(0.5,0.5));
+
+
+  // Set up the initial grid points
+  vector<Vector2D> InitPoints = 
+    cartesian_mesh(50,50,
+		   outer.getBoundary().first,
+		   outer.getBoundary().second);
 
   // Set up the tessellation
   VoronoiMesh tess;
@@ -38,86 +58,56 @@ int main(void)
   RigidWallHydro hbc(rs);
 
   // Set up the equation of state
-  double gamma=5./3.;
-  IdealGas eos(gamma);
+  IdealGas eos(5./3.);
 
   // Set up the point motion scheme
-  Lagrangian l_motion;
-  RoundCells pointmotion(l_motion,hbc);
+  Lagrangian raw_pointmotion;
+  RoundCells pointmotion(raw_pointmotion, hbc);  
 
   // Set up the interpolation
   LinearGaussConsistent interpolation(eos,outer,hbc);
 
   // Set up the initial Hydro
-  double rho=1;
-  double high_radius=0.3;
-  double x_velocity=0;
-  double y_velocity=0;
-  Uniform2D density(rho);
-  Circle hot_spot(Vector2D(0,0),high_radius);
+  Uniform2D density(1);
+  Circle hot_spot(Vector2D(0,0),0.3);
   Uniform2D low_pressure(1);
   Uniform2D high_pressure(100);
   Piecewise pressure(hot_spot,
 		     high_pressure,
 		     low_pressure);
-  Uniform2D xvelocity(x_velocity);
-  Uniform2D yvelocity(y_velocity);
+  Uniform2D xvelocity(0);
+  Uniform2D yvelocity(0);
 
   // Set up the external source term
-  double center_mass=10;
-  double softening_scale=0.05;
-  CenterGravity gravity(center_mass,softening_scale);
-  ConservativeForce force(gravity);
+  ZeroForce force;
 
   // Set up the simulation
   hdsim sim(InitPoints,tess,interpolation,density,pressure,xvelocity,
 	    yvelocity,eos,rs,pointmotion,force,outer,hbc);
 
   // Choose the Courant number
-  double cfl=3;
-  sim.SetCfl(cfl);
+  sim.SetCfl(3);
 
   // How long shall we run the simulation?
-  double tend=0.05;
+  const double tend=0.05;
   sim.SetEndTime(tend);
 
-  // Custom output criteria
-  double output_dt=0.01;
-  double last_dump_time=0;
-  int dump_number=0;
-
-  // Run main loop of the sim
-  while(sim.GetTime()<tend)
-    {
-      try
-	{
-	  // This purely for user feedback
-	  if(sim.GetCycle()%25==0)
-	    cout<<"Sim time is "<<sim.GetTime()<<" Step number "<<sim.GetCycle()<<endl;
-
-	  // Custom output criteria
-	  if((sim.GetTime()-last_dump_time)>output_dt)
-	    {
-	      last_dump_time=sim.GetTime();
-	      ++dump_number;
-	      write_snapshot_to_hdf5(sim,"c:\\sim_data\\output"+
-				     int2str(dump_number)+".bin");
-	    }
-
-	  // Advance one time step
-	  sim.TimeAdvance2Mid();
-	}
-      catch(UniversalError const& eo)
-	{
-	  DisplayError(eo);
-	}
-    }
+  // Main loop
+  SafeTimeTermination term_cond(tend,1e6);
+  ProgressReport diag1(25);
+  ConsecutiveSnapshots diag2(0.01);
+  MultipleDiagnostics diag;
+  diag.diag_list.push_back(&diag1);
+  diag.diag_list.push_back(&diag2);
+  simulation2d::main_loop(sim,
+			  term_cond,
+			  &hdsim::TimeAdvance2Mid,
+			  &diag);
 
   // Done running the simulation, output the data
-  string filename="c:\\sim_data\\output.bin";
-  write_snapshot_to_hdf5(sim,filename);
-
-  // We are done!!
+  write_snapshot_to_hdf5(sim,"final.h5");
+	
+  // Announce 
   cout<<"Finished running the simulation"<<endl;
 
   return 0;
