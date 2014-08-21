0529bde0939b3c59fe930e6b5ce2386c36cbec29
almog.yalin <almog.yalin@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-08-21 16:22:00 +0000

    Fixed clang warnings

--- trunk/source/misc/utils.hpp
+++ trunk/source/misc/utils.hpp
@@ -92,29 +92,21 @@ template <class T> vector<T> operator*
 template <typename T>
 T LinearInterpolation(const vector<T> &x,const vector<T> &y,T xi)
 {
-  typename vector<T>::const_iterator it,begin,end;
-  begin=x.begin();
-  end=x.end();
-  it=upper_bound(begin,end,xi);
-  if(it==end)
-    throw UniversalError("X out of range in Linear Interpolation");
-  if(it==begin)
-    throw UniversalError("X out of range in Linear Interpolation");
+  typename vector<T>::const_iterator it=upper_bound(x.begin,x.end,xi);
+  assert(it!=x.end && it!=x.begin && 
+	 "X out of range in Linear Interpolation");
   if(*it==xi)
-    return y[(int)(it-x.begin())];
+    return y[static_cast<size_t>(it-x.begin())];
+
   // Are we near the edge?
-  if(it==end-1)
-    {
-      end=it-1;
-      return y[(int)(it-begin)]+(xi-*it)*(y[(int)(end-begin)]
-					  -y[(int)(it-begin)])/(*end-*it);
-    }
+  if(it==x.end-1)
+    return y[static_cast<size_t>(it-x.begin)]+(xi-*it)*
+      (y[static_cast<size_t>(it-1-x.begin)]-
+       y[static_cast<size_t>(it-x.begin)])/(*(it-1)-*it);
   else
-    {
-      end=it+1;
-      return y[(int)(it-begin)]+(xi-*it)*(y[(int)(end-begin)]
-					  -y[(int)(it-begin)])/(*end-*it);
-    }
+    return y[static_cast<size_t>(it-x.begin)]+(xi-*it)*
+      (y[static_cast<size_t>(it+1-x.begin)]-
+       y[static_cast<size_t>(it-x.begin)])/(*(it+1)-*it);
 }
 
 /*! \brief Returns the minimal term in a vector
