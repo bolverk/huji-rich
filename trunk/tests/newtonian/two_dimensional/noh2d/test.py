4a47806be586ca97a711064e2454602c992f638f
almog.yalin <almog.yalin@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-08-26 18:30:57 +0000

    Adapted noh test for mpi

--- trunk/tests/newtonian/two_dimensional/noh2d/test.py
+++ trunk/tests/newtonian/two_dimensional/noh2d/test.py
@@ -33,34 +33,64 @@ def calc_analytic_p(d0,v0,g,r,t):
     else:
         return 0.5*d0*v0**2*(g+1.0)**2/(g-1.0)
 
+def consolidate_data_single(fname):
+
+    import h5py
+    import numpy
+
+    f = h5py.File(fname)
+
+    res = {}
+    for field in f:
+        res[field] = numpy.array(f[field])
+    return res
+
+def consolidate_data(flist):
+
+    import numpy
+
+    clist = [consolidate_data_single(fname) for fname in flist]
+
+    res = {}
+    for field in clist[0]:
+        res[field] = numpy.concatenate([itm[field] for itm in clist])
+    return res
+
 def main():
 
     import numpy
     import math
     import h5py
+    import glob
 
-    f = h5py.File('final.h5')
+    #f = h5py.File('final.h5')
 
-    x_list = f['x_coordinate']
-    y_list = f['y_coordinate']
-    r_list = [math.sqrt(x**2+y**2) for x,y in zip(x_list,y_list)]
-    d_list = f['density']
-    p_list = f['pressure']
-    vx_list = f['x_velocity']
-    vy_list = f['y_velocity']
-    vm_list = [math.sqrt(vx**2+vy**2) for vx,vy in zip(vx_list, vy_list)]
-    t = numpy.array(f['time'])[0]
+    #x_list = f['x_coordinate']
+    #y_list = f['y_coordinate']
+    #r_list = [math.sqrt(x**2+y**2) for x,y in zip(x_list,y_list)]
+    #d_list = f['density']
+    #p_list = f['pressure']
+    #vx_list = f['x_velocity']
+    #vy_list = f['y_velocity']
+    #vm_list = [math.sqrt(vx**2+vy**2) for vx,vy in zip(vx_list, vy_list)]
+    #t = numpy.array(f['time'])[0]
+    data = consolidate_data(glob.glob('final*.h5'))
+    data['radius'] = numpy.sqrt(data['x_coordinate']**2+
+                                data['y_coordinate']**2)
+    data['speed'] = numpy.sqrt(data['x_velocity']**2+
+                               data['y_velocity']**2)
+    t = data['time'][0]
     g = numpy.loadtxt('adiabatic_index.txt')
     vc = numpy.loadtxt('collapse_velocity.txt')
     di = numpy.loadtxt('initial_density.txt')
 
-    analytic_d = [calc_analytic_d(di,vc,g,r,t) for r in r_list]
-    analytic_p = [calc_analytic_p(di,vc,g,r,t) for r in r_list]
-    analytic_v = [calc_analytic_v(di,vc,g,r,t) for r in r_list]
+    analytic_d = [calc_analytic_d(di,vc,g,r,t) for r in data['radius']]
+    analytic_p = [calc_analytic_p(di,vc,g,r,t) for r in data['radius']]
+    analytic_v = [calc_analytic_v(di,vc,g,r,t) for r in data['radius']]
 
-    l1_density = l1_comparison(analytic_d, d_list)
-    l1_pressure = l1_comparison(analytic_p, p_list)
-    l1_velocity = l1_comparison(analytic_v, vm_list)
+    l1_density = l1_comparison(analytic_d, data['density'])
+    l1_pressure = l1_comparison(analytic_p, data['pressure'])
+    l1_velocity = l1_comparison(analytic_v, data['speed'])
 
     f = open('gradesheet.txt','w')
     f.write(str(l1_density)+'\n')
