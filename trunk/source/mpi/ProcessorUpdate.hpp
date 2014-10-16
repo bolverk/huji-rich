f9d69875349d73394192379910a6c00824c22801
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-10-16 21:01:18 +0000

    Fixed bug

--- trunk/source/mpi/ProcessorUpdate.hpp
+++ trunk/source/mpi/ProcessorUpdate.hpp
@@ -1,6 +1,6 @@
 /*! \file ProcessorUpdate.hpp
-  \brief Abstract class for motion of the processor points
-  \author Elad Steinberg
+\brief Abstract class for motion of the processor points
+\author Elad Steinberg
 */
 #ifndef PROCUPDATE
 #define PROCUPDATE 1
@@ -16,22 +16,23 @@
 class ProcessorUpdate
 {
 public:
-  /*!
-    \brief Moves the processor tessellation
-    \param tproc The tessellation of the processors
-    \param tlocal The tessellation of the local mesh points
-  */
-  virtual void Update(Tessellation &tproc,Tessellation const& tlocal)const=0;
+	/*!
+	\brief Moves the processor tessellation
+	\param tproc The tessellation of the processors
+	\param tlocal The tessellation of the local mesh points
+	*/
+	virtual void Update(Tessellation &tproc,Tessellation const& tlocal)const=0;
 
-  //! \brief virtual destructor
-  virtual ~ProcessorUpdate(void);
-
-  /*!
-  \brief Calcualtes the load imbalance as max(number of points per proc)/(avg per proc)
-  \param tlocal The local tesselaltion
-  \return The load imbalance
-  */
-  double GetLoadImbalance(Tessellation const& tlocal)const;
+	//! \brief virtual destructor
+	virtual ~ProcessorUpdate(void);
+#ifdef RICH_MPI
+	/*!
+	\brief Calcualtes the load imbalance as max(number of points per proc)/(avg per proc)
+	\param tlocal The local tesselaltion
+	\return The load imbalance
+	*/
+	double GetLoadImbalance(Tessellation const& tlocal)const;
+#endif
 };
 
 #endif //PROCUPDATE
