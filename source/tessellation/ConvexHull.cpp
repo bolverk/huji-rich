960445eea006b8c32085ebfe18693d680d4a0f33
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2014-11-04 04:07:22 +0000

    Made ConvexHull more robust

--- source/tessellation/ConvexHull.cpp
+++ source/tessellation/ConvexHull.cpp
@@ -7,25 +7,30 @@ namespace
 {
 	bool VectorSort(Vector2D const& v1,Vector2D const& v2)
 	{
-	  return ((v1.y<v2.y)||((fabs(v1.y-v2.y)<DBL_EPSILON)&&(v1.x<v2.x)));
+		const double relval=max(max(fabs(v1.y),fabs(v2.y)),DBL_EPSILON*2);
+		const double diffmax=4*max(fabs(v1.y-v2.y),fabs(v2.y-v1.y))/relval;
+		if(diffmax<DBL_EPSILON)
+			return v1.x<v2.x;
+		else
+			return v1.y<v2.y;
 	}
 
-  /*
+	/*
 	bool AngleSort(Vector2D const& v1,Vector2D const& v2)
 	{
-		const double tol=1e-8;
-		const double a1=atan2(v1.y,v1.x);
-		const double a2=atan2(v2.y,v2.x);
-		if(a1<a2+tol)
-			return true;
-		if(a2<a1+tol)
-			return false;
-		if(abs(v1)<abs(v2))
-			return true;
-		else
-			return false;
+	const double tol=1e-8;
+	const double a1=atan2(v1.y,v1.x);
+	const double a2=atan2(v2.y,v2.x);
+	if(a1<a2+tol)
+	return true;
+	if(a2<a1+tol)
+	return false;
+	if(abs(v1)<abs(v2))
+	return true;
+	else
+	return false;
 	}
-  */
+	*/
 
 	Vector2D GetReflectedPoint(Edge const& edge,Vector2D const& point)
 	{
@@ -82,19 +87,19 @@ void ConvexHull(vector<Vector2D> &result,Tessellation const* tess,int index)
 	vector<Vector2D> pfirst,plast;
 	for(size_t i=1;i<(size_t)n-1;++i)
 	{
-	  if(abs(angles[(size_t)indeces[i]]-angles[(size_t)indeces[0]])<tol)
-	    pfirst.push_back(points[(size_t)indeces[i]+1]);
-	  if(abs(angles[(size_t)indeces[(size_t)n-i-2]]-angles[(size_t)indeces[(size_t)n-2]])<tol)
-	    plast.push_back(points[(size_t)indeces[(size_t)n-i-2]+1]);
+		if(abs(angles[(size_t)indeces[i]]-angles[(size_t)indeces[0]])<tol)
+			pfirst.push_back(points[(size_t)indeces[i]+1]);
+		if(abs(angles[(size_t)indeces[(size_t)n-i-2]]-angles[(size_t)indeces[(size_t)n-2]])<tol)
+			plast.push_back(points[(size_t)indeces[(size_t)n-i-2]+1]);
 	}
 	result[0]=points[0];
 	if(!pfirst.empty())
 	{
-	  pfirst.insert(pfirst.begin(),points[(size_t)indeces[0]+1]);
+		pfirst.insert(pfirst.begin(),points[(size_t)indeces[0]+1]);
 		int N=(int)pfirst.size();
 		vector<double> dist((size_t)N);
 		for(int i=0;i<N;++i)
-		  dist[(size_t)i]=abs(pfirst[(size_t)i]-points[0]);
+			dist[(size_t)i]=abs(pfirst[(size_t)i]-points[0]);
 		vector<int> indeces2((size_t)N);
 		sort_index(dist,indeces2);
 		ReArrangeVector(pfirst,indeces2);
@@ -103,21 +108,21 @@ void ConvexHull(vector<Vector2D> &result,Tessellation const* tess,int index)
 	}
 	if(!plast.empty())
 	{
-	  plast.insert(plast.begin(),points[(size_t)indeces[(size_t)n-2]+1]);
+		plast.insert(plast.begin(),points[(size_t)indeces[(size_t)n-2]+1]);
 		int N=(int)plast.size();
 		vector<double> dist;
 		for(int i=0;i<N;++i)
-		  dist.push_back(abs(plast[(size_t)i]-points[0]));
+			dist.push_back(abs(plast[(size_t)i]-points[0]));
 		vector<int> indeces2((size_t)N);
 		sort_index(dist,indeces2);
 		ReArrangeVector(plast,indeces2);
 		for(int i=0;i<N;++i)
-		  result[(size_t)(n-1-i)]=plast[(size_t)i];
+			result[(size_t)(n-1-i)]=plast[(size_t)i];
 	}
 	int loc1=(int)pfirst.size();
 	int loc2=(int)plast.size();
 	for(int i=loc1+1;i<n-loc2;++i)
-	  result[(size_t)i]=points[(size_t)indeces[(size_t)i-1]+1];
+		result[(size_t)i]=points[(size_t)indeces[(size_t)i-1]+1];
 }
 
 void ConvexEdges(vector<int> &result,Tessellation const* tess,int index)
@@ -129,7 +134,7 @@ void ConvexEdges(vector<int> &result,Tessellation const* tess,int index)
 	vector<double> angles((size_t)nedges);
 	for(int i=0;i<nedges;++i)
 	{
-	  Edge const& edge=tess->GetEdge(edges[(size_t)i]);
+		Edge const& edge=tess->GetEdge(edges[(size_t)i]);
 		const int other=(edge.neighbors.first==index)? edge.neighbors.second : edge.neighbors.first;
 		Vector2D otherpoint=(other==-1) ? GetReflectedPoint(edge,mypoint) : tess->GetMeshPoint(other);
 		angles[(size_t)i]=atan2(otherpoint.y-mypoint.y,otherpoint.x-mypoint.x);
@@ -137,5 +142,5 @@ void ConvexEdges(vector<int> &result,Tessellation const* tess,int index)
 	vector<int> temp;
 	sort_index(angles,temp);
 	for(size_t i=0;i<(size_t)nedges;++i)
-	  result[i]=edges[(size_t)temp[i]];
+		result[i]=edges[(size_t)temp[i]];
 }
