68232561e5bf1f3138da0c2ffce5f6588a3595b8
almog.yalin@gmail.com <almog.yalin@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-04 15:59:52 +0000

    added function for generating the extensive conserved variables

---
+++ trunk/source/newtonian/three_dimensional/extensive_generator.hpp
@@ -0,0 +1,31 @@
+#ifndef EXTENSIVE_GENERATOR_HPP
+#define EXTENSIVE_GENERATOR_HPP 1
+
+#include "conserved_3d.hpp"
+#include "computational_cell.hpp"
+#include "../common/equation_of_state.hpp"
+#include "../../misc/utils.hpp"
+#include "../../3D/Tessellation/Tessellation3D.hpp"
+
+Conserved3D calc_intensive(const ComputationalCell& cell,
+			 const EquationOfState& eos);
+
+class ExtensiveGenerator: public Index2Member<Conserved3D>
+{
+public:
+
+  ExtensiveGenerator(const vector<ComputationalCell>& cells,
+		     const Tessellation3D& tess,
+		     const EquationOfState& eos);
+
+  size_t getLength(void) const;
+
+  Conserved3D operator()(size_t i) const;
+
+private:
+  const vector<ComputationalCell>& cells_;
+  const Tessellation3D& tess_;
+  const EquationOfState& eos_;
+};
+
+#endif // EXTENSIVE_GENERATOR_HPP
