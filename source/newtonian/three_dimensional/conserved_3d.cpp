f650cc07323303814e909a69778d85c96cdba990
almog.yalin@gmail.com <almog.yalin@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-04 15:55:05 +0000

    Added scalar product to conserved variables

--- source/newtonian/three_dimensional/conserved_3d.cpp
+++ source/newtonian/three_dimensional/conserved_3d.cpp
@@ -14,3 +14,21 @@ Conserved3D::Conserved3D(double mass_i,
 			 const vector<double>& tracers_i):
   mass(mass_i), momentum(momentum_i), 
   energy(energy_i), tracers(tracers_i) {}
+
+namespace {
+  vector<double> operator*(double s, const vector<double>& v)
+  {
+    vector<double> res(v.size());
+    for(size_t i=0;i<v.size();++i)
+      res[i] = s*v[i];
+    return res;
+  }
+}
+
+Conserved3D operator*(double s, const Conserved3D& c)
+{
+  return Conserved3D(s*c.mass,
+		     s*c.momentum,
+		     s*c.energy,
+		     s*c.tracers);
+}
