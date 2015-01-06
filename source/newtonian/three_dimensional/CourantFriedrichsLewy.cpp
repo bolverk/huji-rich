e45d1589ddae22995d6f265959b2c275d19d7738
almog.yalin@gmail.com <almog.yalin@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-06 09:16:37 +0000

    Added implementation to methods needed for time advance

---
+++ source/newtonian/three_dimensional/CourantFriedrichsLewy.cpp
@@ -0,0 +1,45 @@
+#include "CourantFriedrichsLewy.hpp"
+#include "../../misc/utils.hpp"
+
+CourantFriedrichsLewy::CourantFriedrichsLewy(double cfl):
+  cfl_(cfl)
+{
+  assert(cfl_<1 && "cfl number must be smaller than 1");
+}
+
+namespace {
+  class TimeStepBoundCalculator: public Index2Member<double>
+  {
+  public:
+
+    TimeStepBoundCalculator(const Tessellation3D& tess,
+			    const vector<ComputationalCell>& cells,
+			    const EquationOfState& eos):
+      tess_(tess), cells_(cells), eos_(eos) {}
+
+    size_t getLength(void) const
+    {
+      return cells_.size();
+    }
+
+    double operator()(size_t i) const
+    {
+      return tess_.GetWidth(i)/
+	(abs(cells_[i].velocity)+
+	 eos_.dp2c(cells_[i].density,cells_[i].pressure));
+    }
+
+  private:
+    const Tessellation3D& tess_;
+    const vector<ComputationalCell>& cells_;
+    const EquationOfState& eos_;
+  };
+}
+
+double CourantFriedrichsLewy::operator()
+  (const Tessellation3D& tess,
+   const vector<ComputationalCell>& cells,
+   const EquationOfState& eos) const
+{
+  return cfl_*lazy_min(TimeStepBoundCalculator(tess,cells,eos));
+}
---
+++ source/newtonian/three_dimensional/default_cell_updater.hpp
@@ -0,0 +1,23 @@
+/*! \file defualt_cell_updter.hpp
+  \brief Default cell update scheme
+  \author Almog Yalinewich
+ */
+
+#ifndef DEFAULT_CELL_UPDATER_HPP
+#define DEFAULT_CELL_UPDATER_HPP 1
+
+#include "cell_updater.hpp"
+
+class DefaultCellUpdater: public CellUpdater
+{
+public:
+
+  //! \brief Class constructor
+  DefaultCellUpdater(void);
+
+  ComputationalCell operator()
+  (const Conserved3D& intensive,
+   const EquationOfState& eos) const;
+};
+
+#endif // DEFAULT_CELL_UPDATER_HPP
