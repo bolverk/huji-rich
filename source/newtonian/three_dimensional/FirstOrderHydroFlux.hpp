21ee47c99cd01a3c952edef9fc400b1de6c5dbe3
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-11 12:04:25 +0000

    Added first order flux calculator

---
+++ source/newtonian/three_dimensional/FirstOrderHydroFlux.hpp
@@ -0,0 +1,35 @@
+/*! \file FirstOrderHydroFlux.hpp
+  \brief Class for flux calculator for first order hydro. Assumes rigid walls as default.
+  \author Elad Steinberg
+ */
+
+#ifndef FIRST_ORDER_HYDRO_FLUX_HPP
+#define FIRST_ORDER_HYDRO_FLUX_HPP 1
+
+
+#include "flux_calculator.hpp"
+#include "../common/riemann_solver.hpp"
+#include "../../misc/utils.hpp"
+
+class FirstOrderHydroFlux : public FluxCalculator
+{
+private:
+	RiemannSolver const& rs_;
+public:
+	/*!
+	\brief Class constructor
+	\param rs The Riemann solver
+	*/
+	FirstOrderHydroFlux(RiemannSolver const& rs);
+
+	//! \brief Class destructor
+	~FirstOrderHydroFlux(void);
+
+	vector<Conserved3D> operator()(const Tessellation3D& tess,
+		const vector<ComputationalCell>& cells,const EquationOfState& eos,
+		const vector<Vector3D>& point_velocities) const;
+
+
+};
+
+#endif //FIRST_ORDER_HYDRO_FLUX_HPP
