de6d742a976005e294857c53e973d11cb99dc562
eladtan1@gmail.com <eladtan1@gmail.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-12 12:22:54 +0000

    Fixed bug in cross product

--- source/3D/GeometryCommon/Vector3D.cpp
+++ source/3D/GeometryCommon/Vector3D.cpp
@@ -214,7 +214,7 @@ Vector3D CrossProduct(Vector3D const& v1, Vector3D const& v2)
 {
 	double x = v1.y*v2.z - v1.z*v2.y;
 	double y = v1.z*v2.x - v1.x*v2.z;
-	double z = v1.x*v2.y - v1.y*v2.z;
+	double z = v1.x*v2.y - v1.y*v2.x;
 
 	return Vector3D(x, y, z);
 }
