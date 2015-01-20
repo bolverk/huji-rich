0c1e88e33b286c0d463337201f66a63628abf819
almog.yalin <almog.yalin@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-20 05:14:57 +0000

    Added hdf5 dump for voronoi

---
+++ trunk/source/tessellation/hdf5_logger.cpp
@@ -0,0 +1,46 @@
+#include "hdf5_logger.hpp"
+#include "../misc/utils.hpp"
+#include "tessellation.hpp"
+#include "../newtonian/two_dimensional/hdf5_diagnostics.hpp"
+
+HDF5Logger::HDF5Logger(const string& fname):
+  fname_(fname) {}
+
+namespace{
+  class CoordinateExtractor: public Index2Member<double>
+  {
+  public:
+
+    CoordinateExtractor(const Tessellation& tess,
+			double Vector2D::* component):
+      tess_(tess), component_(component) {}
+
+    size_t getLength(void) const
+    {
+      return static_cast<size_t>(tess_.GetPointNo());
+    }
+
+    double operator()(size_t i) const
+    {
+      return (tess_.GetMeshPoint(static_cast<int>(i))).*component_;
+    }
+
+  private:
+    const Tessellation& tess_;
+    double Vector2D::* component_;
+  };
+}
+
+void HDF5Logger::output(const VoronoiMesh& v)
+{
+  HDF5Shortcut hdf5sc(fname_);
+  hdf5sc(string("x"),serial_generate(CoordinateExtractor(reinterpret_cast<const Tessellation&>(v),&Vector2D::x)));
+  hdf5sc(string("y"),serial_generate(CoordinateExtractor(reinterpret_cast<const Tessellation&>(v),&Vector2D::y)));
+}
+
+void HDF5Logger::output(const Tessellation& v)
+{
+  HDF5Shortcut hdf5sc(fname_);
+  hdf5sc("x",serial_generate(CoordinateExtractor(v,&Vector2D::x)));
+  hdf5sc("y",serial_generate(CoordinateExtractor(v,&Vector2D::y)));
+}
