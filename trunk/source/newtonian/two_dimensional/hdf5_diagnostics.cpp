334b721804ac30311718512a2010240b70fbc8f0
almog.yalin <almog.yalin@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-01-21 07:45:24 +0000

    fixed clang warnings

--- trunk/source/newtonian/two_dimensional/hdf5_diagnostics.cpp
+++ trunk/source/newtonian/two_dimensional/hdf5_diagnostics.cpp
@@ -4,59 +4,6 @@
 using namespace H5;
 
 namespace {
-	void write_std_vector_to_hdf5
-  (H5File& file,
-   vector<size_t> const& num_list,
-   string const& caption)
-  {
-    hsize_t dimsf[1];
-    dimsf[0] = static_cast<hsize_t>(num_list.size());
-    DataSpace dataspace(1, dimsf);
-
-    FloatType datatype(PredType::NATIVE_UINT);
-    datatype.setOrder(H5T_ORDER_LE);
-
-    // Modify dataset creation property to enable chunking
-    DSetCreatPropList  plist;
-    if(dimsf[0]>100000)
-      dimsf[0]=100000;
-    plist.setChunk(1,dimsf);
-    plist.setDeflate(6);
-
-    DataSet dataset = file.createDataSet(H5std_string(caption),
-					 datatype,
-					 dataspace,plist);
-
-    const vector<unsigned> buf = list_static_cast<unsigned,size_t>(num_list);
-    dataset.write(&buf[0],PredType::NATIVE_UINT);
-  }
-
-  void write_std_vector_to_hdf5
-  (H5File& file,
-   vector<double> const& num_list,
-   string const& caption)
-  {
-    hsize_t dimsf[1];
-    dimsf[0] = static_cast<hsize_t>(num_list.size());
-    DataSpace dataspace(1, dimsf);
-
-    FloatType datatype(PredType::NATIVE_DOUBLE);
-    datatype.setOrder(H5T_ORDER_LE);
-
-    // Modify dataset creation property to enable chunking
-    DSetCreatPropList  plist;
-    if(dimsf[0]>100000)
-      dimsf[0]=100000;
-    plist.setChunk(1,dimsf);
-    plist.setDeflate(6);
-
-    DataSet dataset = file.createDataSet(H5std_string(caption),
-					 datatype,
-					 dataspace,plist);
-
-    dataset.write(&num_list[0],PredType::NATIVE_DOUBLE);
-  }
-
   vector<double> read_double_vector_from_hdf5(H5File& file,string const& data_name)
   {
     DataSet dataset = file.openDataSet(data_name);
@@ -91,32 +38,6 @@ namespace {
     vector<int> result(static_cast<size_t>(NX));
     dataset.read(&result[0],PredType::NATIVE_INT);
     return result;
-  }
-
-  void write_std_vector_to_hdf5
-  (H5File& file,
-   vector<int> const& num_list,
-   string const& caption)
-  {
-    hsize_t dimsf[1];
-    dimsf[0] = static_cast<hsize_t>(num_list.size());
-    DataSpace dataspace(1, dimsf);
-
-    IntType datatype(PredType::NATIVE_INT);
-    datatype.setOrder(H5T_ORDER_LE);
-
-    // Modify dataset creation property to enable chunking
-    DSetCreatPropList  plist;
-    if(dimsf[0]>100000)
-      dimsf[0]=100000;
-    plist.setChunk(1,dimsf);
-    plist.setDeflate(6);
-
-    DataSet dataset = file.createDataSet(H5std_string(caption),
-					 datatype,
-					 dataspace,plist);
-
-    dataset.write(&num_list[0],PredType::NATIVE_INT);
   }
 }
 
