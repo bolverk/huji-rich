68a1b707a941e548aed43d8b097ffc9392d9ca3f
almog.yalin <almog.yalin@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-02-02 17:25:31 +0000

    Updated consecutive snapshots diagnostics

--- source/newtonian/test_2d/consecutive_snapshots.hpp
+++ source/newtonian/test_2d/consecutive_snapshots.hpp
@@ -6,7 +6,9 @@
 #ifndef CONSECUTIVE_SNAPSHOTS_HPP
 #define CONSECUTIVE_SNAPSHOTS_HPP 1
 
+#include <memory>
 #include "main_loop_2d.hpp"
+#include "index2filename.hpp"
 
 //! \brief A diagnostic class that writes snapshots at regular intervals
 class ConsecutiveSnapshots: public DiagnosticFunction
@@ -18,15 +20,18 @@ public:
     \param init_time Initial time
     \param counter Counts how many times time advance was called
    */
-  ConsecutiveSnapshots(double dt,double init_time=0,int counter=0);
+  ConsecutiveSnapshots(double dt,
+		       Index2FileName* i2fn,
+		       double init_time=0,
+		       int counter=0);
 
   void operator()(hdsim const& sim);
 
 private:
   double next_time_;
-  double last_time_;
   const double dt_;
   int counter_;
+  std::auto_ptr<Index2FileName> i2fn_;
 };
 
 #endif // CONSECUTIVE_SNAPSHOTS_HPP
