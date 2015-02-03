b3a4ac4a20731333de3ee72d9237af3dec0e1d66
itay@zmbq.com <itay@zmbq.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-02-03 13:37:28 +0000

    Leftover from a previous commit

---
+++ branches/Geometry3D/source/3D/GeometryCommon/Tetrahedron.cpp
@@ -0,0 +1,103 @@
+/* \file Tetrahedron.cpp 
+ \brief A simple tetrahedron wrapper
+ \author Itay Zandbank */
+
+#include "Tetrahedron.hpp"
+#include "../Utilities/assert.hpp"
+#include "Mat44.hpp"
+
+using namespace std;
+
+Tetrahedron::Tetrahedron(const std::vector<Vector3D> &vertices) : _vertices(vertices)
+{
+	BOOST_ASSERT(vertices.size() == 4);
+}
+
+Tetrahedron::Tetrahedron(const Vector3D v1, const Vector3D v2, const Vector3D v3, const Vector3D v4) : _vertices(4)
+{
+	_vertices[0] = v1;
+	_vertices[1] = v2;
+	_vertices[2] = v3;
+	_vertices[3] = v4;
+}
+
+Vector3D Tetrahedron::center() const
+{
+	if (!_center.is_initialized())
+		_center = CalculateCenter();
+	return _center.value();
+}
+
+double Tetrahedron::volume() const
+{
+	if (!_volume.is_initialized())
+		_volume = CalculateVolume();
+	return _volume.value();
+}
+
+// \brief Find the circumcenter of a tetrahedron
+// \param vertices - a vector of the 4 corners
+// \returns The circumcenter
+// \remark Taken from here: http://mathworld.wolfram.com/Circumsphere.html
+Vector3D Tetrahedron::CalculateCenter() const
+{
+#define v1 _vertices[0]
+#define v2 _vertices[1]
+#define v3 _vertices[2]
+#define v4 _vertices[3]
+
+	Mat44<double> m_a{ v1.x, v1.y, v1.z, 1,
+		v2.x, v2.y, v2.z, 1,
+		v3.x, v3.y, v3.z, 1,
+		v4.x, v4.y, v4.z, 1 };
+	double a = m_a.determinant();
+
+	Mat44<double> m_Dx = { abs2(v1), v1.y, v1.z, 1,
+		abs2(v2), v2.y, v2.z, 1,
+		abs2(v3), v3.y, v3.z, 1,
+		abs2(v4), v4.y, v4.z, 1 };
+	double Dx = m_Dx.determinant();
+
+	Mat44<double> m_Dy = { abs2(v1), v1.x, v1.z, 1,
+		abs2(v2), v2.x, v2.z, 1,
+		abs2(v3), v3.x, v3.z, 1,
+		abs2(v4), v4.x, v4.z, 1 };
+	double Dy = -m_Dy.determinant();
+
+	Mat44<double> m_Dz = { abs2(v1), v1.x, v1.y, 1,
+		abs2(v2), v2.x, v2.y, 1,
+		abs2(v3), v3.x, v3.y, 1,
+		abs2(v4), v4.x, v4.y, 1 };
+	double Dz = m_Dz.determinant();
+
+	Mat44<double> m_c = { abs2(v1), v1.x, v1.y, v1.z,
+		abs2(v2), v2.x, v2.y, v2.z,
+		abs2(v3), v3.x, v3.y, v3.z,
+		abs2(v4), v4.x, v4.y, v4.z };
+	double c = m_c.determinant();
+
+#undef v1, v2, v3, v4
+
+	return Vector3D(Dx / (2 * a), Dy / (2 * a), Dz / (2 * a));
+
+}
+
+double Tetrahedron::CalculateVolume() const
+{
+	// TODO: Calculate volume
+	return -1;
+}
+
+std::ostream& operator<<(std::ostream &output, const Tetrahedron &tetrahedron)
+{
+	output << "{ ";
+	for (int i = 0; i < 4; i++)
+	{
+		output << tetrahedron[i] << " ";
+		if (i < 3)
+			output << "- ";
+	}
+	output << "}";
+
+	return output;
+}
---
+++ branches/Geometry3D/source/3D/GeometryCommon/Tetrahedron.hpp
@@ -0,0 +1,30 @@
+/* \file Tetrahedron.hpp
+ \brief A simple encapsulation of a tetrahedron 
+ \author Itay Zandbank
+*/
+
+#include <vector>
+#include "Vector3D.hpp"
+#include <boost/optional.hpp>
+
+class Tetrahedron
+{
+private:
+	std::vector<Vector3D> _vertices;
+	mutable boost::optional<Vector3D> _center;   // Caches the center, which doesn't really change the object
+	mutable boost::optional<double> _volume;     // Another cache
+
+	Vector3D CalculateCenter() const;
+	double CalculateVolume() const;
+public:
+	Tetrahedron(const std::vector<Vector3D> &vertices);
+	Tetrahedron(const Vector3D v1, const Vector3D v2, const Vector3D v3, const Vector3D v4);
+
+	Vector3D center() const;
+	double volume() const;
+
+	const vector<Vector3D> vertices() const { return _vertices;  }
+	const Vector3D operator[](int index) const { return _vertices[index]; }
+};
+
+std::ostream& operator<<(std::ostream &output, const Tetrahedron &tetrahedron);
