a2cb04cbc6cc3b2af590069a357e91dae2d99a93
itay@zmbq.com <itay@zmbq.com@558b2e7e-b7dc-760a-655a-ba0e3c3e962e>
2015-02-05 22:13:30 +0000

    Refactor the Voronoi tests.

--- branches/Geometry3D/source/3D/Itay/TestVoroPlusPlus/TestVoronoi.cpp
+++ branches/Geometry3D/source/3D/Itay/TestVoroPlusPlus/TestVoronoi.cpp
@@ -43,42 +43,77 @@ TEST(VoroPlusPlus, FaceStore)
 	}
 }
 
-TEST(VoroPlusPlus, Cube)
-{
-	const int perSide = 5;
 
+double RandomDouble(double min, double max)
+{
+	double f = (double)rand() / RAND_MAX;  // Between 0 and 1,inclusive
+	return (max - min) * f + min;
+}
+
+Vector3D RandomVector(const OuterBoundary3D &boundary)
+{
+	Vector3D v;
+
+	v.x = RandomDouble(boundary.BackLowerLeft().x, boundary.FrontUpperRight().x);
+	v.y = RandomDouble(boundary.BackLowerLeft().y, boundary.FrontUpperRight().y);
+	v.z = RandomDouble(boundary.BackLowerLeft().z, boundary.FrontUpperRight().z);
+
+	return v;
+}
+
+vector<Vector3D> CreateRandom(int numPoints, const OuterBoundary3D &boundary)
+{
+	srand(17);   // Make sure we generate the same sequence over and over
+	vector<Vector3D> points;
+
+	for (int i = 0; i < numPoints; i++)
+	{
+		Vector3D v = RandomVector(boundary);
+		points.push_back(v);
+	}
+
+	return points;
+}
+
+vector<Vector3D> CreateCubeMesh(int perSide)
+{
 	vector<Vector3D> mesh;
 	for (int x = 0; x < perSide; x++)
 		for (int y = 0; y < perSide; y++)
 			for (int z = 0; z < perSide; z++)
 				mesh.push_back(Vector3D(x, y, z));
 
-	VoroPlusPlus tes;
-	RectangularBoundary3D boundary(Vector3D(perSide - 0.5, perSide - 0.5, perSide - 0.5),
-		Vector3D(-0.5, -0.5, -0.5));
-	tes.Initialise(mesh, boundary);
+	return mesh;
+}
 
+void EnsureCubeVoronoi(const vector<Vector3D> &mesh, const Tessellation3D &tes)
+{
 	ASSERT_EQ(tes.GetPointNo(), mesh.size());
 	for (int pt = 0; pt < mesh.size(); pt++)
 	{
-//		cout << mesh[pt].x << ", " << mesh[pt].y << ", " << mesh[pt].z << ": " << endl;
 		auto faces = tes.GetCellFaces(pt);
 		EXPECT_EQ(faces.size(), 6);
 		for (int fc = 0; fc < faces.size(); fc++)
 		{
-//			cout << "\t";
 			auto face = tes.GetFace(fc);
 			EXPECT_EQ(face.vertices.size(), 4);
-/*			for (int i = 0; i < 4; i++)
-				cout << "(" << face.vertices[i].x << ", " << face.vertices[i].y << ", " << face.vertices[i].z << ") ";
-			cout << endl << "\t" << "Area : " << face.GetArea() << endl; */
 			EXPECT_NEAR(face.GetArea(), 1.0, 1e-12);
 		}
 		auto CoM = tes.GetCellCM(pt);
 		EXPECT_EQ(CoM, mesh[pt]);
 		EXPECT_NEAR(tes.GetVolume(pt), 1.0, 1e-12);
-//		cout << "Center: " << CoM.x << ", " << CoM.y << ", " << CoM.z << endl;
 	}
+}
+
+TEST(VoroPlusPlus, Cube)
+{
+	vector<Vector3D> mesh = CreateCubeMesh(5);
+
+	VoroPlusPlus tes;
+	RectangularBoundary3D boundary(Vector3D(4.5, 4.5, 4.5),
+		Vector3D(-0.5, -0.5, -0.5));
+	tes.Initialise(mesh, boundary);
+	EnsureCubeVoronoi(mesh, tes);
 }
 
 void assertion_gtest_bridge(const char *expr, const char *function, const char *file, long line)
