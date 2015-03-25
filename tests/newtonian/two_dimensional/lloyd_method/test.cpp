a2ad37c03545c84f55a49e740434dee15f33e8db
Almog Yalinewich <almog.yalin@gmail.com>
2015-03-25 17:53:36 +0200

    Adapt test

--- tests/newtonian/two_dimensional/lloyd_method/test.cpp
+++ tests/newtonian/two_dimensional/lloyd_method/test.cpp
@@ -22,45 +22,54 @@
 #include "source/newtonian/test_2d/random_pert.hpp"
 #include "source/newtonian/test_2d/main_loop_2d.hpp"
 #include "source/misc/mesh_generator.hpp"
+#include "source/newtonian/two_dimensional/simple_flux_calculator.hpp"
+#include "source/newtonian/two_dimensional/simple_cell_updater.hpp"
 
 using namespace std;
 using namespace simulation2d;
 
 namespace {
+
+  vector<ComputationalCell> calc_init_cond
+  (const Tessellation& tess)
+  {
+    vector<ComputationalCell> res(static_cast<size_t>(tess.GetPointNo()));
+    for(size_t i=0;i<res.size();++i){
+      res[i].density = 1;
+      res[i].pressure = 1;
+      res[i].velocity = Vector2D(0,0);
+    }
+    return res;
+  }
+
   class SimData
   {
   public:
-
     SimData(void):
+      pg_(),
       width_(1),
       np_(10),
       init_points_(RandSquare(np_*np_,0,width_,0,width_)),
       outer_(0, width_, width_, 0),
-      tess_(),
-      interpm_(),
-      density_(1),
-      pressure_(1),
-      xvelocity_(0),
-      yvelocity_(0),
+      tess_(init_points_,outer_),
       eos_(5./3.),
       bpm_(),
       rs_(),
-      hbc_(rs_),
-      point_motion_(bpm_,hbc_),
+      point_motion_(bpm_,eos_),
       force_(),
-      sim_(init_points_,
-	   tess_,
-	   interpm_,
-	   density_,
-	   pressure_,
-	   xvelocity_,
-	   yvelocity_,
+      tsf_(0.3),
+      fc_(rs_),
+      cu_(),
+      sim_(tess_,
+	   outer_,
+	   pg_,
+	   calc_init_cond(tess_),
 	   eos_,
-	   rs_,
 	   point_motion_,
 	   force_,
-	   outer_,
-	   hbc_) {}
+	   tsf_,
+	   fc_,
+	   cu_) {}
 
     hdsim& getSim(void)
     {
@@ -68,31 +77,28 @@ namespace {
     }
 
   private:
-
+    const SlabSymmetry pg_;
     const double width_;
     const int np_;
     const vector<Vector2D> init_points_;
     const SquareBox outer_;
     VoronoiMesh tess_;
-    PCM2D interpm_;
-    const Uniform2D density_;
-    const Uniform2D pressure_;
-    const Uniform2D xvelocity_;
-    const Uniform2D yvelocity_;
     const IdealGas eos_;
     Eulerian bpm_;
     const Hllc rs_;
-    const RigidWallHydro hbc_;
     RoundCells point_motion_;
     ZeroForce force_;
+    const SimpleCFL tsf_;
+    const SimpleFluxCalculator fc_;
+    const SimpleCellUpdater cu_;
     hdsim sim_;
   };
 
   vector<double> volume_list(hdsim const& sim)
   {
-    vector<double> res(sim.GetCellNo(),0);
-    for(int i=0;i<sim.GetCellNo();++i)
-      res[i] = sim.GetCellVolume(i);
+    vector<double> res(sim.getAllCells().size(),0);
+    for(size_t i=0;i<res.size();++i)
+      res[i] = sim.getCellVolume(i);
     return res;
   }
 
