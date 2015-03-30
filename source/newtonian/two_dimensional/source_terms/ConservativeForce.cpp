27d4736804227c4be026503b6dfec0a5d5fe5274
Almog Yalinewich <almog.yalin@gmail.com>
2015-03-30 20:24:58 +0300

    Simplified conservative force

--- source/newtonian/two_dimensional/source_terms/ConservativeForce.cpp
+++ source/newtonian/two_dimensional/source_terms/ConservativeForce.cpp
@@ -6,14 +6,17 @@ using std::sqrt;
 
 namespace {
 
+  /*
   Vector2D remove_parallel_component(const Vector2D& s,
 				     const Vector2D& r)
   {
     return s-r*ScalarProd(r,s)/ScalarProd(r,r);
   }
+  */
 
+  /*
   Vector2D MassFlux(const Tessellation& tess,
-		    const PhysicalGeometry& /*pg*/,
+		    const PhysicalGeometry& pg,
 		    const CacheData& cd,
 		    const vector<Extensive>& fluxes,
 		    int point)
@@ -26,19 +29,12 @@ namespace {
 	const Edge& edge = tess.GetEdge(edge_index[i]);
 	if(point==edge.neighbors.first){
 	  if(edge.neighbors.second>-1){
-	    /*
-	    dm-=pg.calcArea(edge)*fluxes[static_cast<size_t>(edge_index[i])].mass*
-	      (center-tess.GetMeshPoint(edge.neighbors.second));
-	    */
 	    dm-=cd.areas[i]*fluxes[static_cast<size_t>(edge_index[i])].mass*
 	      (center-tess.GetMeshPoint(edge.neighbors.second));
 	  }
 	  else{
 	    const Vector2D p = edge.vertices.second - edge.vertices.first;
 	    const Vector2D r=remove_parallel_component(center - edge.vertices.first,p);
-	    /*
-	    dm-=2*pg.calcArea(edge)*fluxes[static_cast<size_t>(edge_index[i])].mass*r;
-	    */
 	    dm-=2*cd.areas[i]*fluxes[static_cast<size_t>(edge_index[i])].mass*r;
 	  }
 	}
@@ -46,10 +42,6 @@ namespace {
 	  if(point==edge.neighbors.second){
 	    if(edge.neighbors.first>-1)
 	      {
-		/*
-		dm+=pg.calcArea(edge)*fluxes[static_cast<size_t>(edge_index[i])].mass*
-		  (center-tess.GetMeshPoint(edge.neighbors.first));
-		*/
 		dm+=cd.areas[static_cast<size_t>(i)]*
 		  fluxes[static_cast<size_t>(edge_index[i])].mass*
 		  (center-tess.GetMeshPoint(edge.neighbors.first));
@@ -58,7 +50,6 @@ namespace {
 	      {
 		const Vector2D p = edge.vertices.second - edge.vertices.first;
 		const Vector2D r = remove_parallel_component(center - edge.vertices.second, p);
-		//		dm+=2*pg.calcArea(edge)*fluxes[static_cast<size_t>(edge_index[i])].mass*r;
 		dm+=2*cd.areas[static_cast<size_t>(i)]*
 		  fluxes[static_cast<size_t>(edge_index[i])].mass*r;
 	      }
@@ -68,6 +59,7 @@ namespace {
       }
     return dm;
   }
+*/
 }
 
 ConservativeForce::ConservativeForce(const Acceleration& acc):
@@ -77,11 +69,11 @@ ConservativeForce::~ConservativeForce(void){}
 
 vector<Extensive> ConservativeForce::operator()
   (const Tessellation& tess,
-   const PhysicalGeometry& pg,
+   const PhysicalGeometry& /*pg*/,
    const CacheData& cd,
    const vector<ComputationalCell>& cells,
    const vector<Extensive>& fluxes,
-   const vector<Vector2D>& point_velocities,
+   const vector<Vector2D>& /*point_velocities*/,
    const double t) const
 {
   vector<Extensive> res(static_cast<size_t>(tess.GetPointNo()));
@@ -103,10 +95,15 @@ vector<Extensive> ConservativeForce::operator()
     const double volume = cd.volumes[i];
     res[i].mass = 0;
     res[i].momentum = volume*cells[i].density*acc;
+    res[i].energy = volume*cells[i].density*ScalarProd(acc,cells[i].velocity);
+    /*
+    const Vector2D mass_flux = MassFlux(tess,pg,cd,fluxes,static_cast<int>(i));
     res[i].energy = 
       volume*cells[i].density*
       ScalarProd(point_velocities[i],acc)+
-      0.5*ScalarProd(MassFlux(tess,pg,cd,fluxes,static_cast<int>(i)),acc);
+      0.5*ScalarProd(mass_flux,acc);
+    */
+      //      0.5*ScalarProd(MassFlux(tess,pg,cd,fluxes,static_cast<int>(i)),acc);
   }
   return res;
 }
