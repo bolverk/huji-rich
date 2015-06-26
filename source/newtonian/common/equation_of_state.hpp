0950ad0cc129ff4227a5700167e1ba1a8502fd07
Almog Yalinewich <almog.yalin@gmail.com>
2015-06-26 07:43:07 +0300

    add documentation

--- source/newtonian/common/equation_of_state.hpp
+++ source/newtonian/common/equation_of_state.hpp
@@ -20,6 +20,7 @@ public:
   /*! \brief Calculates the thermal energy per unit mass
     \param d Density
     \param p Pressure
+    \param tracers Tracers
     \return Thermal energy per unit mass
    */
   virtual double dp2e(double d, double p, const map<string,double>& tracers=map<string,double>()) const = 0;
@@ -27,6 +28,7 @@ public:
   /*! \brief Calculates the pressure
     \param d Density
     \param e Specific thermal energy
+    \param tracers Tracers
     \return Presusre
    */
   virtual double de2p(double d, double e,
@@ -35,6 +37,7 @@ public:
   /*! \brief Calculates the speed of sound
     \param d Density
     \param e Specific thermal energy
+    \param tracers Tracers
     \return Speed of sound
   */
   virtual double de2c(double d, double e,
@@ -43,6 +46,7 @@ public:
   /*! \brief Calculates the speed of sound
     \param d Density
     \param p Pressure
+    \param tracers Tracers
     \return Speed of sound
    */
   virtual double dp2c(double d, double p,
@@ -51,13 +55,16 @@ public:
   /*! \brief Calculates the entropy per unit mass
     \param d Density
     \param p Pressure
+    \param tracers Tracers
     \return Entropy
   */
   virtual double dp2s(double d, double p,
 		      const map<string,double>& tracers=map<string,double>()) const = 0;
+
   /*! \brief Calculates the pressure from the netropy
     \param s Entropy
     \param d Density
+    \param tracers Tracers
     \return Entropy
   */
   virtual double sd2p(double s, double d,
