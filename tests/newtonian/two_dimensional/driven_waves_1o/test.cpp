fde3959fb8412ab086da4d79a8e08bf2998aba28
Almog Yalinewich <almog.yalin@gmail.com>
2015-08-23 15:50:53 +0300

    make test compile

--- tests/newtonian/two_dimensional/driven_waves_1o/test.cpp
+++ tests/newtonian/two_dimensional/driven_waves_1o/test.cpp
@@ -21,6 +21,9 @@
 #include "source/newtonian/two_dimensional/geometric_outer_boundaries/PeriodicBox.hpp"
 #include "source/newtonian/two_dimensional/hydro_boundary_conditions/PeriodicHydro.hpp"
 #include "source/newtonian/two_dimensional/source_terms/ConservativeForce.hpp"
+#include "source/newtonian/two_dimensional/simple_flux_calculator.hpp"
+#include "source/newtonian/two_dimensional/simple_cell_updater.hpp"
+#include "source/newtonian/two_dimensional/simple_extensive_updater.hpp"
 #include "source/newtonian/test_2d/main_loop_2d.hpp"
 #include "source/newtonian/two_dimensional/hdf5_diagnostics.hpp"
 #include "source/misc/mesh_generator.hpp"
@@ -29,97 +32,113 @@ using namespace std;
 using namespace simulation2d;
 
 namespace {
-class PeriodicDriver: public Acceleration
-{
-public:
-
-  PeriodicDriver(double wavelenth,
-		 double amplitude):
-    k_(2*M_PI/wavelenth),
-    amp_(amplitude) {}
-
-  Vector2D Calculate
-  (Tessellation const& tess,
-   vector<Primitive> const& /*cells*/,
-   int point,
-   vector<Conserved> const& /*fluxes*/,
-   vector<Vector2D> const& /*point_velocity*/,
-   HydroBoundaryConditions const& /*hbc*/,
-   vector<vector<double> > const& /*tracers*/,
-   double /*t*/,
-   double /*dt*/)
+  class PeriodicDriver: public Acceleration
   {
-    const double x = tess.GetMeshPoint(point).x;
+  public:
+
+    PeriodicDriver(double wavelenth,
+		   double amplitude):
+      k_(2*M_PI/wavelenth),
+      amp_(amplitude) {}
+
+    Vector2D operator()
+    (const Tessellation& tess,
+     const vector<ComputationalCell>& /*cells*/,
+     const vector<Extensive>& /*fluxes*/,
+     const double /*time*/,
+     const int point) const
+    {
+      const double x = tess.GetMeshPoint(point).x;
       const double acceleration = amp_*cos(k_*x);
-     return Vector2D(-acceleration,0);
-  }
+      return Vector2D(-acceleration,0);
+    }
 
-private:
-  const double k_;
-  const double amp_;
-};
+  private:
+    const double k_;
+    const double amp_;
+  };
 
-class SimData
-{
-public:
-
-  SimData(void):
-    width_(1),
-    init_points_(cartesian_mesh(30,30,
-				Vector2D(0,0),
-				Vector2D(width_,width_))),
-    outer_(0,width_,width_,0),
-    tess_(),
-    interp_method_(),
-    density_(1),
-    pressure_(1),
-    xvelocity_(0.1),
-    yvelocity_(0),
-    eos_(5./3.),
-    pm_naive_(),
-    rs_(),
-    hbc_(rs_),
-    point_motion_(pm_naive_,hbc_),
-    acc_(1,0.001),
-	force_(acc_),
-    sim_(init_points_,
-	 tess_,
-	 interp_method_,
-	 density_,
-	 pressure_,
-	 xvelocity_,
-	 yvelocity_,
-	 eos_,
-	 rs_,
-	 point_motion_,
-	 force_,
-	 outer_,
-	 hbc_) {}
-
-  hdsim& getSim(void)
+  vector<ComputationalCell> calc_init_cond
+  (const Tessellation& tess)
   {
-    return sim_;
+    ComputationalCell source;
+    source.density = 1;
+    source.pressure = 1;
+    source.velocity = Vector2D(0.1,0);
+    return vector<ComputationalCell>
+      (static_cast<size_t>(tess.GetPointNo()),
+       source);
   }
 
-private:
-  const double width_;
-  const vector<Vector2D> init_points_;
-  const PeriodicBox outer_;
-  VoronoiMesh tess_;
-  PCM2D interp_method_;
-  const Uniform2D density_;
-  const Uniform2D pressure_;
-  const Uniform2D xvelocity_;
-  const Uniform2D yvelocity_;
-  const IdealGas eos_;
-  Lagrangian pm_naive_;
-  const Hllc rs_;
-  const PeriodicHydro hbc_;
-  RoundCells point_motion_;
-  PeriodicDriver acc_;
-  ConservativeForce force_;
-  hdsim sim_;
-};
+  class SimData
+  {
+  public:
+
+    SimData(void):
+      width_(1),
+      init_points_(cartesian_mesh(30,30,
+				  Vector2D(0,0),
+				  Vector2D(width_,width_))),
+      outer_(0,width_,width_,0),
+      tess_(init_points_,outer_),
+      pg_(),
+      interp_method_(),
+      density_(1),
+      pressure_(1),
+      xvelocity_(0.1),
+      yvelocity_(0),
+      eos_(5./3.),
+      pm_naive_(),
+      rs_(),
+      hbc_(rs_),
+      point_motion_(pm_naive_,eos_),
+      acc_(1,0.001),
+      force_(acc_),
+      tsf_(0.3),
+      fc_(rs_),
+      eu_(),
+      cu_(),
+      sim_(tess_,
+	   outer_,
+	   pg_,
+	   calc_init_cond(tess_),
+	   eos_,
+	   point_motion_,
+	   force_,
+	   tsf_,
+	   fc_,
+	   eu_,
+	   cu_) {}
+
+    hdsim& getSim(void)
+    {
+      return sim_;
+    }
+
+  private:
+    const double width_;
+    const vector<Vector2D> init_points_;
+    const PeriodicBox outer_;
+    VoronoiMesh tess_;
+    const SlabSymmetry pg_;
+    PCM2D interp_method_;
+    const Uniform2D density_;
+    const Uniform2D pressure_;
+    const Uniform2D xvelocity_;
+    const Uniform2D yvelocity_;
+    const IdealGas eos_;
+    Lagrangian pm_naive_;
+    const Hllc rs_;
+    const PeriodicHydro hbc_;
+    RoundCells point_motion_;
+    PeriodicDriver acc_;
+    ConservativeForce force_;
+    const SimpleCFL tsf_;
+    const SimpleFluxCalculator fc_;
+    const SimpleExtensiveUpdater eu_;
+    const SimpleCellUpdater cu_;
+    hdsim sim_;
+  };
 
   void my_main_loop(hdsim& sim)
   {
