42ba863109eff100395bde5c6da2a3f153d68e7f
Elad Steinberg <eladtan1@gmail.com>
2015-08-31 17:06:41 +0300

    Added freeflow ghost point generator

---
+++ source/newtonian/two_dimensional/ghost_point_generators/FreeFlowGhostGenerator.cpp
@@ -0,0 +1,23 @@
+#include "FreeFlowGhostGenerator.hpp"
+
+std::map<size_t, ComputationalCell> FreeFlowGenerator::operator() (const Tessellation& tess,
+	const vector<ComputationalCell>& cells) const
+{
+	vector<std::pair<size_t, size_t> > outer_edges=GetOuterEdgesIndeces(tess);
+	std::map<size_t, ComputationalCell> res;
+	for (size_t i = 0; i < outer_edges.size(); ++i)
+	{
+		Edge const& edge = tess.GetEdge(static_cast<int>(outer_edges[i].first));
+		int real_cell = outer_edges[i].second == 1 ? edge.neighbors.second : edge.neighbors.first;
+		res.insert(std::pair<size_t, ComputationalCell>(static_cast<size_t>(real_cell),
+			cells[static_cast<size_t>(real_cell)]));
+	}
+	return res;
+}
+
+std::pair<ComputationalCell, ComputationalCell> FreeFlowGenerator::GetGhostGradient(Tessellation const& tess,
+	vector<ComputationalCell> const& /*cells*/, vector<std::pair<ComputationalCell, ComputationalCell> > const& gradients,
+	size_t ghost_index)const
+{
+	return gradients[static_cast<size_t>(tess.GetOriginalIndex(static_cast<int>(ghost_index)))];
+}
---
+++ source/newtonian/two_dimensional/ghost_point_generators/FreeFlowGhostGenerator.hpp
@@ -0,0 +1,25 @@
+/*! \file FreeFlowGhostenerator.hpp
+\brief Class for creating computationalcells of ghost points for free flow
+\author Elad Steinberg
+*/
+
+#ifndef FREEFLOW_GENERATOR_HPP
+#define FREEFLOW_GENERATOR_HPP 1
+
+#include "../GhostPointGenerator.hpp"
+
+/*! \brief Class for creating computational cells of ghost points for free flow
+\author Elad Steinberg
+*/
+class FreeFlowGenerator : public GhostPointGenerator
+{
+public:
+	std::map<size_t, ComputationalCell> operator() (const Tessellation& tess,
+		const vector<ComputationalCell>& cells) const;
+
+	std::pair<ComputationalCell, ComputationalCell> GetGhostGradient(Tessellation const& tess,
+		vector<ComputationalCell> const& cells, vector<std::pair<ComputationalCell, ComputationalCell> > const& gradients,
+		size_t ghost_index)const;
+};
+
+#endif // FREEFLOW_GENERATOR_HPP
