e31e4cda45bd0a947019417b12a07b7f88512ac1
Almog Yalinewich <almog.yalin@gmail.com>
2015-09-03 18:56:12 +0300

    create class for regular flux

--- source/newtonian/two_dimensional/condition_action_sequence.cpp
+++ source/newtonian/two_dimensional/condition_action_sequence.cpp
@@ -1,4 +1,5 @@
 #include "condition_action_sequence.hpp"
+#include "simple_flux_calculator.hpp"
 
 ConditionActionSequence::ConditionActionSequence
 (const vector<pair<const Condition*,const Action*> >& sequence):
@@ -57,3 +58,66 @@ vector<Extensive> ConditionActionSequence::operator()
 ConditionActionSequence::Condition::~Condition(void) {}
 
 ConditionActionSequence::Action::~Action(void) {}
+
+RegularFlux::RegularFlux(const RiemannSolver& rs):
+  rs_(rs) {}
+
+namespace {
+  Extensive conserved_to_extensive
+  (const Conserved& c, const ComputationalCell& cell)
+  {
+    Extensive res;
+    res.mass = c.Mass;
+    res.momentum = c.Momentum;
+    res.energy = c.Energy;
+    for(boost::container::flat_map<string,double>::const_iterator it=
+	  cell.tracers.begin();
+	it!=cell.tracers.end();++it)
+      res.tracers[it->first] = (it->second)*c.Mass;
+    return res;
+  }
+
+  Vector2D normalize(const Vector2D& v)
+  {
+    return v/abs(v);
+  }
+}
+
+Extensive RegularFlux::operator()
+(const Edge& edge,
+ const Tessellation& tess,
+ const vector<Vector2D>& point_velocities,
+ const vector<ComputationalCell>& cells,
+ const EquationOfState& eos,
+ const bool /*aux*/) const
+{
+  assert(edge.neighbors.first>=0 && edge.neighbors.first<tess.GetPointNo());
+  assert(edge.neighbors.second>=0 && edge.neighbors.second<tess.GetPointNo());
+  const Vector2D p = normalize
+    (edge.vertices.second -
+     edge.vertices.first);
+  const Vector2D n = normalize
+    (tess.GetMeshPoint(edge.neighbors.second)-
+     tess.GetMeshPoint(edge.neighbors.first));
+  const double v =
+    ScalarProd
+    (n,
+     tess.CalcFaceVelocity
+     (point_velocities[static_cast<size_t>(edge.neighbors.first)],
+      point_velocities[static_cast<size_t>(edge.neighbors.second)],
+      tess.GetCellCM(edge.neighbors.first),
+      tess.GetCellCM(edge.neighbors.second),
+      calc_centroid(edge)));
+  const Conserved c = rotate_solve_rotate_back
+    (rs_,
+     convert_to_primitive
+     (cells.at(static_cast<size_t>(edge.neighbors.first)),eos),
+     convert_to_primitive
+     (cells.at(static_cast<size_t>(edge.neighbors.second)),eos),
+     v,n,p);
+  return conserved_to_extensive
+    (c,
+     c.Mass>0 ?
+     cells.at(static_cast<size_t>(edge.neighbors.first)):
+     cells.at(static_cast<size_t>(edge.neighbors.second)));	      
+}
--- source/newtonian/two_dimensional/condition_action_sequence.hpp
+++ source/newtonian/two_dimensional/condition_action_sequence.hpp
@@ -2,6 +2,7 @@
 #define CONDITION_ACTION_SEQUENCE_HPP 1
 
 #include "flux_calculator_2d.hpp"
+#include "../common/riemann_solver.hpp"
 
 class ConditionActionSequence: public FluxCalculator
 {
@@ -51,6 +52,25 @@ public:
 
 private:
    const vector<pair<const Condition*, const Action*> > sequence_;
+};
+
+class RegularFlux: public ConditionActionSequence::Action
+{
+public:
+
+  RegularFlux(const RiemannSolver& rs);
+
+  Extensive operator()
+  (const Edge& edge,
+   const Tessellation& tess,
+   const vector<Vector2D>& point_velocities,
+   const vector<ComputationalCell>& cells,
+   const EquationOfState& eos,
+   const bool aux) const;
+
+private:
+
+  const RiemannSolver& rs_;
 };
 
 #endif // CONDITION_ACTION_SEQUENCE_HPP
