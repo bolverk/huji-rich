0affff15b866eb3e2fd05196b024514a5a120c9b
Almog Yalinewich <almog.yalin@gmail.com>
2015-09-03 13:09:42 +0300

    adapt test

--- tests/newtonian/two_dimensional/hydrostatic_moving_mesh_2/test.cpp
+++ tests/newtonian/two_dimensional/hydrostatic_moving_mesh_2/test.cpp
@@ -10,23 +10,40 @@
 #include "source/newtonian/common/hllc.hpp"
 #include "source/newtonian/common/ideal_gas.hpp"
 #include "source/tessellation/VoronoiMesh.hpp"
-#include "source/newtonian/two_dimensional/interpolations/pcm2d.hpp"
 #include "source/newtonian/two_dimensional/spatial_distributions/uniform2d.hpp"
 #include "source/newtonian/two_dimensional/point_motions/eulerian.hpp"
 #include "source/newtonian/two_dimensional/point_motions/round_cells.hpp"
 #include "source/newtonian/two_dimensional/source_terms/zero_force.hpp"
 #include "source/newtonian/two_dimensional/geometric_outer_boundaries/SquareBox.hpp"
-#include "source/newtonian/two_dimensional/hydro_boundary_conditions/RigidWallHydro.hpp"
 #include "source/newtonian/two_dimensional/diagnostics.hpp"
 #include "source/misc/utils.hpp"
-#include "source/newtonian/two_dimensional/interpolations/linear_gauss_consistent.hpp"
 #include "source/newtonian/test_2d/main_loop_2d.hpp"
 #include "source/misc/mesh_generator.hpp"
+#include "source/newtonian/two_dimensional/modular_flux_calculator.hpp"
+#include "source/newtonian/two_dimensional/simple_extensive_updater.hpp"
+#include "source/newtonian/two_dimensional/simple_cell_updater.hpp"
+#include "source/newtonian/two_dimensional/idle_hbc.hpp"
+#include "source/newtonian/two_dimensional/ghost_point_generators/RigidWallGenerator.hpp"
+#include "source/newtonian/two_dimensional/interpolations/LinearGaussImproved.hpp"
 
 using namespace std;
 using namespace simulation2d;
 
 namespace {
+
+  vector<ComputationalCell> calc_init_cond
+  (const Tessellation& tess)
+  {
+    vector<ComputationalCell> res
+      (static_cast<size_t>(tess.GetPointNo()));
+    for(size_t i=0;i<res.size();++i){
+      res[i].density = 1;
+      res[i].pressure = 1;
+      res[i].velocity = Vector2D(0,0);
+    }
+    return res;
+  }
+
   class SimData
   {
   public:
@@ -38,31 +55,33 @@ namespace {
 				  Vector2D(0,0),
 				  Vector2D(width_,width_))),
       outer_(0,width_,width_,0),
-      tess_(),
+      tess_(init_points_,outer_),
       eos_(5./3.),
-      interpm_(eos_,outer_,hbc_,true,false),
       density_(1),
       pressure_(1),
       xvelocity_(0),
       yvelocity_(0),
       bpm_(),
       rs_(),
-      hbc_(rs_),
-      point_motion_(bpm_,hbc_),
+      gpg_(),
+      sr_(eos_,gpg_),
+      point_motion_(bpm_,eos_),
       force_(),
-      sim_(init_points_,
-	   tess_,
-	   interpm_,
-	   density_,
-	   pressure_,
-	   xvelocity_,
-	   yvelocity_,
+      tsf_(0.3),
+      hbc_(),
+      fc_(gpg_,sr_,rs_,hbc_),
+      eu_(),
+      sim_(tess_,
+	   outer_,
+	   pg_,
+	   calc_init_cond(tess_),
 	   eos_,
-	   rs_,
 	   point_motion_,
 	   force_,
-	   outer_,
-	   hbc_) {}
+	   tsf_,
+	   fc_,
+	   eu_,
+	   cu_) {}
 
     hdsim& getSim(void)
     {
@@ -75,26 +94,32 @@ namespace {
     const int np_;
     const vector<Vector2D> init_points_;
     const SquareBox outer_;
+    const SlabSymmetry pg_;
     VoronoiMesh tess_;
     const IdealGas eos_;
-    LinearGaussConsistent interpm_;
     const Uniform2D density_;
     const Uniform2D pressure_;
     const Uniform2D xvelocity_;
     const Uniform2D yvelocity_;
     Eulerian bpm_;
     const Hllc rs_;
-    const RigidWallHydro hbc_;
+    const RigidWallGenerator gpg_;
+    const LinearGaussImproved sr_;
     RoundCells point_motion_;
     ZeroForce force_;
+    const SimpleCFL tsf_;
+    const IdleHBC hbc_;
+    const ModularFluxCalculator fc_;
+    const SimpleExtensiveUpdater eu_;
+    const SimpleCellUpdater cu_;
     hdsim sim_;
   };
 
   vector<double> volume_list(hdsim const& sim)
   {
-    vector<double> res(sim.GetCellNo(),0);
-    for(int i=0;i<sim.GetCellNo();++i)
-      res[i] = sim.GetCellVolume(i);
+    vector<double> res(sim.getAllCells().size(),0);
+    for(size_t i=0;i<res.size();++i)
+      res[i] = sim.getCellVolume(i);
     return res;
   }
 
@@ -127,7 +152,7 @@ namespace {
     WriteMinMaxVolume diag("res.txt");
     main_loop(sim,
 	      term_cond,
-	      &hdsim::TimeAdvance2Mid,
+	      &hdsim::TimeAdvance2Heun,
 	      &diag);
   }
 }
