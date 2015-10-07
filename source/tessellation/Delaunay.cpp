0a77d7a7fbc3f28566146955b9c6bf2b64d46553
Almog Yalinewich <almog.yalin@gmail.com>
2015-10-07 14:55:59 +0300

    remove redundant variable

--- source/tessellation/Delaunay.cpp
+++ source/tessellation/Delaunay.cpp
@@ -758,10 +758,9 @@ vector<vector<int> > Delaunay::FindOuterPointsMPI
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	vector<vector<int> > res(edges.size());
-	vector<int> res_temp, f_temp, f_add(f.size(), 0);
+	vector<int> f_temp, f_add(f.size(), 0);
 	if (olength>100)
 	{
-	  res_temp.reserve(static_cast<size_t>(20 * sqrt(static_cast<double>(olength))));
 		// Walk to an outer point
 	  int cur_facet = static_cast<int>(Walk(olength));
 		// Find the real point
