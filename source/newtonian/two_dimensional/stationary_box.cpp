53caf52b0fdae6ae8bfbc963753b53e0f419330c
Almog Yalinewich <almog.yalin@gmail.com>
2015-10-28 10:03:36 +0200

    add stationry box

---
+++ source/newtonian/two_dimensional/stationary_box.cpp
@@ -0,0 +1,38 @@
+#include "stationary_box.hpp"
+
+StationaryBox::StationaryBox(void) {}
+
+namespace {
+  bool is_outer_edge
+  (const Tessellation& tess,
+   const Edge& edge)
+  {
+    const int n1 =
+      tess.GetOriginalIndex
+       (edge.neighbors.first);
+    const int n2 =
+      tess.GetOriginalIndex
+       (edge.neighbors.second);
+    return n1==n2;
+  }
+}
+
+vector<Vector2D> StationaryBox::operator()
+(const Tessellation& tess,
+ const vector<Vector2D>& point_velocities) const
+{
+  const vector<Edge>& edge_list = tess.getAllEdges();
+  vector<Vector2D> res(edge_list.size());
+  for(size_t i=0;i<res.size();++i){
+    const Edge& edge = edge_list.at(i);
+    res.at(i) = is_outer_edge(tess,edge) ?
+      Vector2D() :
+      tess.CalcFaceVelocity
+      (point_velocities.at(static_cast<size_t>(edge.neighbors.first)),
+       point_velocities.at(static_cast<size_t>(edge.neighbors.second)),
+       tess.GetMeshPoint(edge.neighbors.first),
+       tess.GetMeshPoint(edge.neighbors.second),
+       calc_centroid(edge));
+  }
+  return res;
+}
---
+++ source/newtonian/two_dimensional/stationary_box.hpp
@@ -0,0 +1,17 @@
+#ifndef STATIONARY_BOX_HPP
+#define STATIONARY_BOX_HPP 1
+
+#include "edge_velocity_calculator.hpp"
+
+class StationaryBox: public EdgeVelocityCalculator
+{
+public:
+
+  StationaryBox(void);
+
+  vector<Vector2D> operator()
+  (const Tessellation& tess,
+   const vector<Vector2D>& point_velocities) const;
+};
+
+#endif // STATIONARY_BOX_HPP
