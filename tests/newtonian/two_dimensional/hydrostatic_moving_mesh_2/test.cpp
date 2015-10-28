906f37090bf28d45abcc1068f7c4e3ea00ebd554
Almog Yalinewich <almog.yalin@gmail.com>
2015-10-28 09:50:34 +0200

    adapt test

--- tests/newtonian/two_dimensional/hydrostatic_moving_mesh_2/test.cpp
+++ tests/newtonian/two_dimensional/hydrostatic_moving_mesh_2/test.cpp
@@ -25,6 +25,7 @@
 #include "source/newtonian/two_dimensional/idle_hbc.hpp"
 #include "source/newtonian/two_dimensional/ghost_point_generators/RigidWallGenerator.hpp"
 #include "source/newtonian/two_dimensional/interpolations/LinearGaussImproved.hpp"
+#include "source/newtonian/two_dimensional/stationary_box.hpp"
 
 using namespace std;
 using namespace simulation2d;
@@ -70,7 +71,7 @@ namespace {
       force_(),
       tsf_(0.3),
       hbc_(),
-      fc_(gpg_,sr_,rs_,hbc_),
+      fc_(sr_,rs_,hbc_),
       eu_(),
       sim_(tess_,
 	   outer_,
