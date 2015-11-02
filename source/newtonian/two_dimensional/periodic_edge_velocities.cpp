01be92092242089765ca509edc411ead798443e1
Almog Yalinewich <almog.yalin@gmail.com>
2015-11-02 14:48:11 +0200

    add missing files

---
+++ source/newtonian/two_dimensional/periodic_edge_velocities.cpp
@@ -0,0 +1,21 @@
+#include "periodic_edge_velocities.hpp"
+
+PeriodicEdgeVelocities::PeriodicEdgeVelocities(void) {}
+
+vector<Vector2D> PeriodicEdgeVelocities::operator()
+  (const Tessellation& tess,
+   const vector<Vector2D>& point_velocities) const
+{
+  const vector<Edge>& edge_list = tess.getAllEdges();
+  vector<Vector2D> res(edge_list.size());
+  for(size_t i=0;i<res.size();++i){
+    const Edge& edge = edge_list.at(i);
+    res.at(i) = tess.CalcFaceVelocity
+      (point_velocities.at(static_cast<size_t>(tess.GetOriginalIndex(edge.neighbors.first))),
+       point_velocities.at(static_cast<size_t>(tess.GetOriginalIndex(edge.neighbors.second))),
+       tess.GetMeshPoint(tess.GetOriginalIndex(edge.neighbors.first)),
+       tess.GetMeshPoint(tess.GetOriginalIndex(edge.neighbors.second)),
+       calc_centroid(edge));
+  }
+  return res;
+}
---
+++ source/newtonian/two_dimensional/periodic_edge_velocities.hpp
@@ -0,0 +1,17 @@
+#ifndef PERIODIC_EDGE_VELOCITIES_HPP
+#define PERIODIC_EDGE_VELOCITIES_HPP 1
+
+#include "edge_velocity_calculator.hpp"
+
+class PeriodicEdgeVelocities: public EdgeVelocityCalculator
+{
+public:
+
+  PeriodicEdgeVelocities(void);
+
+  vector<Vector2D> operator()
+  (const Tessellation& tess,
+   const vector<Vector2D>& point_velocities) const;
+};
+
+#endif // PERIODIC_EDGE_VELOCITIES_HPP
