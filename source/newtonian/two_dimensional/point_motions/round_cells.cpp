21fe12e51685c82ecdf2b13edbbbead68f7fee09
Elad Steinberg <eladtan1@gmail.com>
2015-11-04 14:31:05 +0200

    Fixed warnings

--- source/newtonian/two_dimensional/point_motions/round_cells.cpp
+++ source/newtonian/two_dimensional/point_motions/round_cells.cpp
@@ -103,7 +103,7 @@ vector<Vector2D> RoundCells::operator()(const Tessellation& tess, const vector<C
 	return res;
 }
 
-vector<Vector2D> RoundCells::ApplyFix(Tessellation const& tess, vector<ComputationalCell> const& cells, double time,
+vector<Vector2D> RoundCells::ApplyFix(Tessellation const& tess, vector<ComputationalCell> const& /*cells*/, double /*time*/,
 	double dt, vector<Vector2D>const & velocities)const
 {
 #ifdef RICH_MPI
