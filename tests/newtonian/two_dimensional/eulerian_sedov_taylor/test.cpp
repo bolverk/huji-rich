3cf2a7ad689d9c409e288645ecd490d7f36e962d
Almog Yalinewich <almog.yalin@gmail.com>
2015-11-12 12:22:21 +0200

    create eulerian sedov taylor test

---
+++ tests/newtonian/two_dimensional/eulerian_sedov_taylor/test.cpp
@@ -0,0 +1,248 @@
+#ifdef RICH_MPI
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/collectives.hpp>
+#endif
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include "source/tessellation/geometry.hpp"
+#include "source/newtonian/two_dimensional/hdsim2d.hpp"
+#include "source/tessellation/tessellation.hpp"
+#include "source/newtonian/common/hllc.hpp"
+#include "source/newtonian/common/ideal_gas.hpp"
+#include "source/tessellation/VoronoiMesh.hpp"
+#include "source/newtonian/two_dimensional/spatial_distributions/uniform2d.hpp"
+#include "source/newtonian/two_dimensional/point_motions/eulerian.hpp"
+#include "source/newtonian/two_dimensional/point_motions/round_cells.hpp"
+#include "source/newtonian/two_dimensional/source_terms/zero_force.hpp"
+#include "source/newtonian/two_dimensional/geometric_outer_boundaries/SquareBox.hpp"
+#include "source/newtonian/test_2d/random_pert.hpp"
+#include "source/newtonian/two_dimensional/diagnostics.hpp"
+#include "source/misc/simple_io.hpp"
+#include "source/misc/mesh_generator.hpp"
+#include "source/newtonian/test_2d/main_loop_2d.hpp"
+#include "source/newtonian/two_dimensional/hdf5_diagnostics.hpp"
+#include "source/tessellation/shape_2d.hpp"
+#include "source/newtonian/test_2d/piecewise.hpp"
+#include "source/newtonian/two_dimensional/simple_flux_calculator.hpp"
+#include "source/newtonian/two_dimensional/simple_cell_updater.hpp"
+#include "source/newtonian/two_dimensional/simple_extensive_updater.hpp"
+#include "source/newtonian/two_dimensional/stationary_box.hpp"
+
+using namespace std;
+using namespace simulation2d;
+
+namespace {
+
+#ifdef RICH_MPI
+
+  vector<Vector2D> process_positions(const SquareBox& boundary)
+  {
+    const Vector2D lower_left = boundary.getBoundary().first;
+    const Vector2D upper_right = boundary.getBoundary().second;
+	const boost::mpi::communicator world;
+    vector<Vector2D> res(static_cast<size_t>(world.size()));
+    if(world.rank()==0)
+	{
+      res = RandSquare(world.size(),
+		       lower_left.x,upper_right.x,
+		       lower_left.y,upper_right.y);
+    }
+	boost::mpi::broadcast(world, res, 0);
+    return res;
+  }
+
+  boost::array<double, 4> FindMaxEdges(Tessellation const& tess)
+  {
+	  const boost::mpi::communicator world;
+	  const int rank = world.rank();
+	  const vector<int>& edge_index = tess.GetCellEdges(rank);
+	  const int n = static_cast<int>(edge_index.size());
+	  boost::array<double, 4> res;
+	  res[0] = min(tess.GetEdge(edge_index[0]).vertices.first.x,
+		  tess.GetEdge(edge_index[0]).vertices.second.x);
+	  res[1] = max(tess.GetEdge(edge_index[0]).vertices.first.x,
+		  tess.GetEdge(edge_index[0]).vertices.second.x);
+	  res[2] = min(tess.GetEdge(edge_index[0]).vertices.first.y,
+		  tess.GetEdge(edge_index[0]).vertices.second.y);
+	  res[3] = max(tess.GetEdge(edge_index[0]).vertices.first.y,
+		  tess.GetEdge(edge_index[0]).vertices.second.y);
+	  for (size_t i = 1; i<static_cast<size_t>(n); ++i)
+	  {
+		  res[0] = min(min(tess.GetEdge(edge_index[i]).vertices.first.x,
+			  tess.GetEdge(edge_index[i]).vertices.second.x), res[0]);
+		  res[1] = max(max(tess.GetEdge(edge_index[i]).vertices.first.x,
+			  tess.GetEdge(edge_index[i]).vertices.second.x), res[1]);
+		  res[2] = min(min(tess.GetEdge(edge_index[i]).vertices.first.y,
+			  tess.GetEdge(edge_index[i]).vertices.second.y), res[2]);
+
+		  res[3] = max(max(tess.GetEdge(edge_index[i]).vertices.first.y,
+			  tess.GetEdge(edge_index[i]).vertices.second.y), res[3]);
+	  }
+	  return res;
+  }
+
+  vector<Vector2D> SquareMeshM(int nx, int ny, Tessellation const& tess,
+	  Vector2D const&lowerleft, Vector2D const&upperright)
+  {
+	  const double widthx = (upperright.x - lowerleft.x) / static_cast<double>(nx);
+	  const double widthy = (upperright.y - lowerleft.y) / static_cast<double>(ny);
+	  const boost::array<double, 4> tessEdges = FindMaxEdges(tess);
+	  nx = static_cast<int>(floor((tessEdges[1] - tessEdges[0]) / widthx + 0.5));
+	  ny = static_cast<int>(floor((tessEdges[3] - tessEdges[2]) / widthy + 0.5));
+	  vector<Vector2D> res;
+	  res.reserve(static_cast<size_t>(nx*ny));
+	  const int nx0 = static_cast<int>(floor((tessEdges[0] - lowerleft.x) / widthx + 0.5));
+	  const int ny0 = static_cast<int>(floor((tessEdges[2] - lowerleft.y) / widthy + 0.5));
+	  Vector2D point;
+	  const boost::mpi::communicator world;
+	  int rank = world.rank();
+	  vector<Vector2D> cpoints;
+	  ConvexHull(cpoints, tess, rank);
+	  for (int i = 0; i<nx; i++)
+	  {
+		  for (int j = 0; j<ny; j++)
+		  {
+			  point.x = (static_cast<double>(i) + 0.5 + nx0)*widthx + lowerleft.x;
+			  point.y = (static_cast<double>(j) + 0.5 + ny0)*widthy + lowerleft.y;
+			  if ((point.x<lowerleft.x) || (point.x>upperright.x) ||
+				  (point.y<lowerleft.y) || (point.y>upperright.y))
+				  continue;
+			  if (PointInCell(cpoints, point))
+				  res.push_back(point);
+		  }
+	  }
+	  return res;
+  }
+#endif
+
+  vector<ComputationalCell> calc_init_cond(const Tessellation& tess)
+  {
+    vector<ComputationalCell> res(static_cast<size_t>(tess.GetPointNo()));
+    for(size_t i=0;i<res.size();++i){
+      res[i].density = 1;
+      res[i].pressure = 
+	abs(tess.GetMeshPoint(static_cast<int>(i)))<0.06 ? 1e4 : 0.01;
+      res[i].velocity = Vector2D(0,0);
+    }
+    return res;
+  }
+
+  class SimData
+  {
+  public:
+
+    SimData(void):
+      pg_(),
+      width_(1),
+      outer_(0,width_,width_,0),
+#ifdef RICH_MPI
+	  vproc_(process_positions(outer_),outer_),
+		init_points_(SquareMeshM(50,50,vproc_,outer_.getBoundary().first,outer_.getBoundary().second)),
+		tess_(vproc_,init_points_,outer_),
+#else
+      init_points_(cartesian_mesh(50,50,outer_.getBoundary().first,
+				  outer_.getBoundary().second)),
+		tess_(init_points_, outer_),
+#endif
+      eos_(5./3.),
+      point_motion_(),
+      sb_(),
+      rs_(),
+      force_(),
+      tsf_(0.3),
+      fc_(rs_),
+      eu_(),
+      cu_(),
+      sim_(
+#ifdef RICH_MPI
+		  vproc_,
+#endif
+		  tess_,
+	   outer_,
+	   pg_,
+	   calc_init_cond(tess_),
+	   eos_,
+	   point_motion_,
+	   sb_,
+	   force_,
+	   tsf_,
+	   fc_,
+	   eu_,
+	   cu_) {}
+
+    hdsim& getSim(void)
+    {
+      return sim_;
+    }
+
+  private:
+    const SlabSymmetry pg_;
+    const double width_;
+    const SquareBox outer_;
+#ifdef RICH_MPI
+	VoronoiMesh vproc_;
+#endif
+    const vector<Vector2D> init_points_;
+    VoronoiMesh tess_;
+    const IdealGas eos_;
+    Eulerian point_motion_;
+    const StationaryBox sb_;
+    const Hllc rs_;
+    ZeroForce force_;
+    const SimpleCFL tsf_;
+    const SimpleFluxCalculator fc_;
+    const SimpleExtensiveUpdater eu_;
+    const SimpleCellUpdater cu_;
+    hdsim sim_;
+  };
+
+  /*  void my_main_loop(hdsim& sim)
+  {
+    SafeTimeTermination term_cond(0.04,1e6);
+#ifdef BUGMODE
+	boost::mpi::communicator world;
+	WriteData diag("process_temp_" + int2str(world.rank()) + ".h5");
+#else
+    WriteTime diag("time.txt");
+#endif
+    main_loop(sim,
+	      term_cond,
+	      &hdsim::TimeAdvance,
+	      &diag);
+  }
+  */
+}
+
+int main(void)
+{
+#ifdef RICH_MPI
+	boost::mpi::environment env;
+	boost::mpi::communicator world;
+#endif
+  SimData sim_data;
+  hdsim& sim = sim_data.getSim();
+
+  //my_main_loop(sim);
+  for (size_t i = 0; i < 100; ++i)
+  {
+	  sim.TimeAdvance();
+#ifdef RICH_MPI
+	  write_snapshot_to_hdf5(sim, "snap_"+int2str(sim.getCycle())+"_"+int2str(world.rank())+".h5");
+#else
+	  write_snapshot_to_hdf5(sim, "snap_" + int2str(sim.getCycle()) + ".h5");
+#endif
+  }
+
+#ifdef RICH_MPI
+  write_snapshot_to_hdf5(sim, "process_"+int2str(world.rank())+"_final"+".h5");
+#else
+  write_snapshot_to_hdf5(sim, "final.h5");
+#endif
+
+
+  return 0;
+}
+
