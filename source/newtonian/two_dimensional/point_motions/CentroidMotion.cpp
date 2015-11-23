f7851c9f3829b3bb77406ac454f7059b50932dc2
Almog Yalinewich <almog.yalin@gmail.com>
2015-11-23 14:28:37 +0200

    fix clang warnings

--- source/newtonian/two_dimensional/point_motions/CentroidMotion.cpp
+++ source/newtonian/two_dimensional/point_motions/CentroidMotion.cpp
@@ -4,64 +4,6 @@
 
 namespace
 {
-	Vector2D FixInDomain(OuterBoundary const& obc, Vector2D &point)
-	{
-		Vector2D res;
-		if (point.x > obc.GetGridBoundary(Right))
-		{
-			point.x -= obc.GetGridBoundary(Right) - obc.GetGridBoundary(Left);
-			res.x += obc.GetGridBoundary(Right) - obc.GetGridBoundary(Left);
-		}
-		if (point.x < obc.GetGridBoundary(Left))
-		{
-			point.x += obc.GetGridBoundary(Right) - obc.GetGridBoundary(Left);
-			res.x -= obc.GetGridBoundary(Right) - obc.GetGridBoundary(Left);
-		}
-		if (point.y > obc.GetGridBoundary(Up))
-		{
-			point.y -= obc.GetGridBoundary(Up) - obc.GetGridBoundary(Down);
-			res.y += obc.GetGridBoundary(Up) - obc.GetGridBoundary(Down);
-		}
-		if (point.y < obc.GetGridBoundary(Down))
-		{
-			point.y += obc.GetGridBoundary(Up) - obc.GetGridBoundary(Down);
-			res.y -= obc.GetGridBoundary(Up) - obc.GetGridBoundary(Down);
-		}
-		return res;
-	}
-
-	vector<Vector2D> GetChull(vector<Vector2D> const& points, Vector2D const& cm)
-	{
-		// Start building the convexhull
-		size_t n = points.size();
-		vector<double> angles(n);
-		for (size_t i = 0; i<n; ++i)
-			angles.at(i) = atan2(points.at(i).y - cm.y, points.at(i).x - cm.x);
-		const vector<size_t> indeces = sort_index(angles);
-		return VectorValues(points, indeces);
-	}
-
-	vector<Vector2D> BuildCell(vector<Vector2D> const& chull, Vector2D const& point,double R)
-	{
-		Vector2D inter;
-		vector<Vector2D> res(chull.size());
-		vector<Edge> edges(chull.size());
-		for (size_t i = 0; i < chull.size(); ++i)
-		{
-			const Vector2D normal = chull[i] - point;
-			Vector2D parallel(normal.y, -normal.x);
-			parallel = parallel/abs(parallel);
-			const Vector2D mid = 0.5*(chull[i] + point);
-			edges[i] = Edge(mid + parallel*(R*100), mid -(R*100)*parallel, 0, 0);
-		}
-		for (size_t i = 0; i < edges.size(); ++i)
-		{
-			if (!SegmentIntersection(edges[i], edges[(i + 1) % edges.size()], res[i]))
-				throw UniversalError("Can't build single voronoi cell");
-		}
-		return res;
-	}
-
 	Vector2D GetCM(vector<Vector2D> const& chull,Vector2D const& meshpoint)
 	{
 		double area = 0;
@@ -147,8 +89,8 @@ namespace
 	}
 }
 
-CentroidMotion::CentroidMotion(double reduction_factor, OuterBoundary const & outer, size_t niter) :
-	reduce_factor_(reduction_factor),outer_(outer),niter_(niter){}
+CentroidMotion::CentroidMotion(double reduction_factor, size_t niter) :
+	reduce_factor_(reduction_factor),niter_(niter){}
 
 vector<Vector2D> CentroidMotion::operator()(const Tessellation & tess, const vector<ComputationalCell>& cells, double /*time*/) const
 {
--- source/newtonian/two_dimensional/point_motions/CentroidMotion.hpp
+++ source/newtonian/two_dimensional/point_motions/CentroidMotion.hpp
@@ -20,10 +20,9 @@ public:
 
 	/*! \brief Class constructor
 	\param reduction_factor The factor to reduce the correction velocity (1/reduction_factor is the number of iterations to fix)
-	\param outer The outer boudnary conditions, used for preventing points from getting outside the box. If periodic then no fix is applied
 	\param niter The number of correction iterations to apply
 	*/
-	CentroidMotion(double reduction_factor,OuterBoundary const& outer, size_t niter = 2);
+	CentroidMotion(double reduction_factor, size_t niter = 2);
 
 	vector<Vector2D> operator()(const Tessellation& tess, const vector<ComputationalCell>& cells, double time) const;
 
@@ -32,7 +31,7 @@ public:
 private:
 
 	const double reduce_factor_;
-	OuterBoundary const& outer_;
+  //	OuterBoundary const& outer_;
 	const size_t niter_;
 };
 
