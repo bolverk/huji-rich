99bc76a574105452159add02e09c0c7468c0cdbd
eladtan <eladtan1@gmail.com>
2015-11-23 15:40:18 +0200

    Added new tests

---
+++ tests/newtonian/two_dimensional/amrconserve2/test.py
@@ -0,0 +1,23 @@
+def main():
+
+    import numpy
+    import math
+    import h5py
+    import glob
+	
+    with h5py.File('final.h5','r+') as f:
+        density_list = numpy.array(f['hydrodynamic']['density'])
+        chi_2 = numpy.sqrt(sum((density_list-1)**2))/len(density_list)
+    mass = numpy.loadtxt('mass.txt')
+    return chi_2<1e-10 and abs(mass-4)<1e-10
+
+ 
+if __name__ == '__main__':
+
+    import os
+
+    if main():
+        os.system('touch test_passed.res')
+    else:
+        os.system('touch test_failed.res')
+
