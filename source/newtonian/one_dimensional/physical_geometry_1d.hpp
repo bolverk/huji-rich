150c371bef751bf9bb2d611a226cc1a9c4a568ca
Almog Yalinewich <almog.yalin@gmail.com>
2016-05-06 16:34:35 +0300

    add physical geometry files for 1d

---
+++ source/newtonian/one_dimensional/physical_geometry_1d.hpp
@@ -0,0 +1,65 @@
+/*! \file physical_geomtry_1d.hpp
+  \author Almog Yalinewich
+  \brief Physical geometry of the grid
+*/
+
+#ifndef PHYSICAL_GEOMETRY_1D
+#define PHYSICAL_GEOMETRY_1D
+
+//! \brief Base class for physical geometry
+class PhysicalGeometry1D
+{
+public:
+
+  /*! \brief Calculates the area
+    \param radius Radius
+    \return Area
+   */
+  virtual double calcArea(double radius) const = 0;
+
+  /*! \brief Calculates the volume
+    \param radius Radius
+    \return Volume
+   */
+  virtual double calcVolume(double radius) const = 0;
+};
+
+//! \brief Planar geometry
+class SlabSymmetry1D: public PhysicalGeometry1D
+{
+public:
+
+  //! \brief Class constructor
+  SlabSymmetry1D(void);
+
+  double calcArea(double radius) const;
+
+  double calcVolume(double radius) const;
+};
+
+//! \brief Cylindrical geometry
+class CylindricalSymmetry1D: public PhysicalGeometry1D
+{
+public:
+
+  //! \brief Class constructor
+  CylindricalSymmetry1D(void);
+
+  double calcArea(double radius) const;
+
+  double calcVolume(double radius) const;
+};
+
+//! \brief Spherical geometry
+class SphericalSymmetry1D: public PhysicalGeometry1D
+{
+public:
+  //! \brief Class constructor
+  SphericalSymmetry1D(void);
+
+  double calcArea(double radius) const;
+
+  double calcVolume(double radius) const;
+};
+
+#endif // PHYSICAL_GEOMETRY_1D
