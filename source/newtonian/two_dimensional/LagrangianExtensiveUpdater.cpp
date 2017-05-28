7056f531d6155cb1cf937cf0e616502b2e1be824
eladtan <eladtan1@gmail.com>
2017-05-28 10:33:34 +0300

    Update LagrangianExtensiveUpdater.cpp
    Removed text that wasn't necessary

--- source/newtonian/two_dimensional/LagrangianExtensiveUpdater.cpp
+++ source/newtonian/two_dimensional/LagrangianExtensiveUpdater.cpp
@@ -1,9 +1,8 @@
 #include "LagrangianExtensiveUpdater.hpp"
 #include "../../misc/utils.hpp"
 
-LagrangianExtensiveUpdater::LagrangianExtensiveUpdater(LagrangianFlux const& fc, ExtensiveUpdater const& beu,
-	EquationOfState const& eos)
-	:fc_(fc),beu_(beu),eos_(eos) {}
+LagrangianExtensiveUpdater::LagrangianExtensiveUpdater(LagrangianFlux const& fc, ExtensiveUpdater const& beu)
+	:fc_(fc),beu_(beu) {}
 
 void LagrangianExtensiveUpdater::operator()
 (const vector<Extensive>& fluxes,
@@ -16,13 +15,11 @@ vector<Extensive>& extensives,
 double time, TracerStickerNames const& ts) const
 { 
 	beu_(fluxes, pg, tess, dt, cd, cells, extensives,time, ts);
+	vector<Extensive> extensives_local(extensives);
 	size_t Npoints = static_cast<size_t>(tess.GetPointNo());
-	vector<double> R(Npoints, 0);
 	Extensive toadd(extensives[0]);
 	size_t t_index=static_cast<size_t>(binary_find(ts.tracer_names.begin(),ts.tracer_names.end(),string("Temperature"))-ts.tracer_names.begin());
 	assert(t_index<ts.tracer_names.size());
-	for (size_t i = 0; i < Npoints; ++i)
-		R[i] = tess.GetWidth(static_cast<int>(i));
 	const vector<Edge>& edge_list = tess.getAllEdges();
 	vector<double> dV(tess.GetPointNo(), 0);
 	vector<double> dv_ws(dV);
@@ -48,30 +45,16 @@ double time, TracerStickerNames const& ts) const
 		Edge const& edge = tess.GetEdge(static_cast<int>(i));
 		if (tess.GetOriginalIndex(edge.neighbors.first) == tess.GetOriginalIndex(edge.neighbors.second))
 			continue;
-		double Cs = 0;
-		Vector2D n = normalize(tess.GetMeshPoint(edge.neighbors.second) - tess.GetMeshPoint(edge.neighbors.first));
-		Cs = eos_.dp2c(cells[static_cast<size_t>(edge.neighbors.first)].density,
-			cells[static_cast<size_t>(edge.neighbors.first)].pressure,cells[static_cast<size_t>(edge.neighbors.first)].tracers,ts.tracer_names) 
-			+ std::abs(fc_.edge_vel_[i] - ScalarProd(n,cells[static_cast<size_t>(edge.neighbors.first)].velocity));
-		Cs = std::max(Cs,eos_.dp2c(cells[static_cast<size_t>(edge.neighbors.second)].density,
-			cells[static_cast<size_t>(edge.neighbors.second)].pressure,cells[static_cast<size_t>(edge.neighbors.first)].tracers,ts.tracer_names) 
-		+ std::abs(fc_.edge_vel_[i]	- ScalarProd(n, cells[static_cast<size_t>(edge.neighbors.second)].velocity)));
-		double density_ratio = cells[static_cast<size_t>(edge.neighbors.first)].density /
-			cells[static_cast<size_t>(edge.neighbors.second)].density;
-		density_ratio = std::max(density_ratio, 1.0 / density_ratio);
-		double p_ratio = cells[static_cast<size_t>(edge.neighbors.first)].pressure /
-			cells[static_cast<size_t>(edge.neighbors.second)].pressure;
-		p_ratio = std::max(p_ratio, 1.0 / p_ratio);
 		double L = cd.areas[i];
-		if (edge.neighbors.first < tess.GetPointNo() && cells[static_cast<size_t>(edge.neighbors.first)].stickers[t_index]<2.5e8)
+		if (edge.neighbors.first < tess.GetPointNo())
 		{
-			ReplaceExtensive(toadd, extensives[static_cast<size_t>(edge.neighbors.first)]);
+			ReplaceExtensive(toadd, extensives_local[static_cast<size_t>(edge.neighbors.first)]);
 			toadd *= (L*(ws)*dt / (cd.volumes[edge.neighbors.first] + dV[edge.neighbors.first] - dv_ws[edge.neighbors.first]));
 			extensives[static_cast<size_t>(edge.neighbors.first)] -= toadd; 
 		}
-		if (edge.neighbors.second < tess.GetPointNo() && cells[static_cast<size_t>(edge.neighbors.second)].stickers[t_index]<2.5e8)
+		if (edge.neighbors.second < tess.GetPointNo())
 		{
-			ReplaceExtensive(toadd, extensives[static_cast<size_t>(edge.neighbors.second)]);
+			ReplaceExtensive(toadd, extensives_local[static_cast<size_t>(edge.neighbors.second)]);
 			toadd *= (L*(ws)*dt / (cd.volumes[edge.neighbors.second] + dV[edge.neighbors.second] + dv_ws[edge.neighbors.second]));
 			extensives[static_cast<size_t>(edge.neighbors.second)] += toadd;
 		}
