67795d502a85efeae3522123777adae2c4a8b0c5
Elad Steinberg <eladtan1@gmail.com>
2018-02-01 09:54:45 -0500

    Removed uneeded output

--- source/newtonian/two_dimensional/LagrangianExtensiveUpdater.cpp
+++ source/newtonian/two_dimensional/LagrangianExtensiveUpdater.cpp
@@ -1,6 +1,6 @@
 #include "LagrangianExtensiveUpdater.hpp"
 #include "../../misc/utils.hpp"
-
+#include <iostream>
 LagrangianExtensiveUpdater::LagrangianExtensiveUpdater(LagrangianFlux const& fc, ExtensiveUpdater const& beu)
 	:fc_(fc),beu_(beu) {}
 
@@ -15,15 +15,19 @@ vector<Extensive>& extensives,
 double time, TracerStickerNames const& ts) const
 { 
 	beu_(fluxes, pg, tess, dt, cd, cells, extensives,time, ts);
-	vector<Extensive> extensives_local(extensives);
-	Extensive toadd(extensives[0]);
-	size_t t_index=static_cast<size_t>(binary_find(ts.tracer_names.begin(),ts.tracer_names.end(),string("Temperature"))-ts.tracer_names.begin());
-	assert(t_index<ts.tracer_names.size());
 	const vector<Edge>& edge_list = tess.getAllEdges();
-	vector<double> dV(tess.GetPointNo(), 0);
-	vector<double> dv_ws(dV);
 	fc_.edge_vel_.resize(edge_list.size(),0);
 	fc_.ws_.resize(edge_list.size(),0);
+	size_t N = tess.GetPointNo();
+	vector<double> dV(N, 0);
+	vector<double> dv_ws(dV);
+	
+	vector<double> areas(edge_list.size(),0),volumes(tess.GetPointNo(),0);
+	for (size_t i = 0; i<edge_list.size(); ++i)
+		areas[i]=cd.areas[i];
+	for (size_t i = 0; i<tess.GetPointNo(); ++i)
+		volumes[i]=cd.volumes[i];
+	
 	for (size_t i = 0; i<edge_list.size(); ++i)
 	{
 		const Edge& edge = edge_list[i];
@@ -38,6 +42,30 @@ double time, TracerStickerNames const& ts) const
 			dv_ws[static_cast<size_t>(edge.neighbors.second)] -= cd.areas[i] * dt*fc_.ws_[i];
 		}
 	}
+	for(size_t i=0;i<N;++i)
+	{
+		//if(cells[i].tracers[t_index]<2.5e8)
+		{
+			assert((cd.volumes[i]+dV[i]+dv_ws[i])>0);
+			double a = (cd.volumes[i]+dV[i])/(cd.volumes[i]+dV[i]+dv_ws[i]);
+			assert(std::isfinite(a));
+			assert(a>0);
+			amax=std::max(amax,a);
+			amin=std::min(amin,a);
+			extensives[i]*=a;
+		}
+	}
+	double ares;
+	MPI_Reduce(&amax,&ares,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
+	if(rank==0)
+		std::cout<<"max a "<<ares<<std::endl;
+	MPI_Reduce(&amin,&ares,1,MPI_DOUBLE,MPI_MIN,0,MPI_COMM_WORLD);
+	if(rank==0)
+		std::cout<<"min a "<<ares<<std::endl;
+	MPI_Barrier(MPI_COMM_WORLD);
+//	if(rank==0)
+	//	std::cout<<"finished beu3"<<std::endl;
+	/*
 	for (size_t i = 0; i < edge_list.size(); ++i)
 	{
 		double ws = fc_.ws_[i];
@@ -45,22 +73,18 @@ double time, TracerStickerNames const& ts) const
 		if (tess.GetOriginalIndex(edge.neighbors.first) == tess.GetOriginalIndex(edge.neighbors.second))
 			continue;
 		double L = cd.areas[i];
-		if (edge.neighbors.first < tess.GetPointNo())
+		if (edge.neighbors.first < tess.GetPointNo() && cells[edge.neighbors.first].tracers[t_index]<2.75e8)
 		{
 			ReplaceExtensive(toadd, extensives_local[static_cast<size_t>(edge.neighbors.first)]);
-			toadd *= (L*(ws)*dt / (cd.volumes[static_cast<size_t>(edge.neighbors.first)] + 
-					       dV[static_cast<size_t>(edge.neighbors.first)] - 
-					       dv_ws[static_cast<size_t>(edge.neighbors.first)]));
-			extensives[static_cast<size_t>(edge.neighbors.first)] -= toadd; 
+			toadd *= (L*(ws)*dt / (cd.volumes[edge.neighbors.first] + dV[edge.neighbors.first] + dv_ws[edge.neighbors.first]));
+			extensives[static_cast<size_t>(edge.neighbors.first)] += toadd; 
 		}
-		if (edge.neighbors.second < tess.GetPointNo())
+		if (edge.neighbors.second < tess.GetPointNo() && cells[edge.neighbors.first].tracers[t_index]<2.75e8)
 		{
 			ReplaceExtensive(toadd, extensives_local[static_cast<size_t>(edge.neighbors.second)]);
-			toadd *= (L*(ws)*dt / (cd.volumes[static_cast<size_t>(edge.neighbors.second)] + 
-					       dV[static_cast<size_t>(edge.neighbors.second)] + 
-					       dv_ws[static_cast<size_t>(edge.neighbors.second)]));
+			toadd *= (L*(ws)*dt / (cd.volumes[edge.neighbors.second] + dV[edge.neighbors.second] + dv_ws[edge.neighbors.second]));
 			extensives[static_cast<size_t>(edge.neighbors.second)] += toadd;
 		}
-	}
+	}*/
 	fc_.Reset();
 }
