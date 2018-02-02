207b58430a7bc89ceb58668dd2d1d860e046b3b8
eladtan <eladtan1@gmail.com>
2018-02-02 09:24:21 -0500

    Improved linear interpolation

--- source/misc/utils.hpp
+++ source/misc/utils.hpp
@@ -128,17 +128,30 @@ template <class T> vector<T> operator*
   \return f(xi)
 */
 template <typename T>
+T LinearInterpolation(const vector<T> &x, const vector<T> &y, T xi);
+
+template <typename T>
 T LinearInterpolation(const vector<T> &x, const vector<T> &y, T xi)
 {
 	typename vector<T>::const_iterator it = upper_bound(x.begin(), x.end(), xi);
-	assert(it != x.end() && it != x.begin() &&
-		"X out of range in Linear Interpolation");
+	if (it == x.end())
+	{
+		std::cout << "X too large in LinearInterpolation, x_i " << xi << " max X " << x.back() << std::endl;
+		throw;
+	}
+	if (it == x.begin())
+	{
+		if (*it < x.at(0))
+		{
+			std::cout << "X too small in LinearInterpolation, x_i " << xi << " min X " << x.at(0) << std::endl;
+			throw;
+		}
+	}
 	if (*it == xi)
-		return y[static_cast<size_t>(it - x.begin())];
+		return y[static_cast<std::size_t>(it - x.begin())];
 
-	return y[static_cast<size_t>(it - x.begin())] + (xi - *it)*
-		(y[static_cast<size_t>(it - 1 - x.begin())] -
-			y[static_cast<size_t>(it - x.begin())]) / (*(it - 1) - *it);
+	return y[static_cast<std::size_t>(it - x.begin())] + (xi - *it)*	(y[static_cast<std::size_t>(it - 1 - x.begin())] 
+		- y[static_cast<std::size_t>(it - x.begin())]) / (*(it - 1) - *it);
 }
 
 /*! \brief Returns the minimal term in a vector
