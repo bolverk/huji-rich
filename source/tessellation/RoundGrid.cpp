2e6eb16dcadfd8665b12c5dae6521e56cd1da681
Bolverk <almog.yalin@gmail.com>
2021-06-16 09:20:48 -0400

    reduce code

--- source/tessellation/RoundGrid.cpp
+++ source/tessellation/RoundGrid.cpp
@@ -15,6 +15,15 @@ namespace {
       d/eta_/R<0.95 ? Vector2D(0,0) : chi_*0.5*(s-r);
     return tess.GetMeshPoint(i) + dw;
   }
+
+  vector<Vector2D> genNewPoints(Tessellation& tess)
+  {
+    const int N = tess.GetPointNo();
+    vector<Vector2D> res(static_cast<size_t>(N));
+    for(int i=0;i<N;++i)
+      res.at(static_cast<size_t>(i)) = genNewPoint(tess,i);
+    return res;
+  }
 }
 
 vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
@@ -33,14 +42,9 @@ vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
 #endif
   vector<Vector2D> res
     (static_cast<size_t>(tess->GetPointNo()));
-  for(int j=0,N=tess->GetPointNo();j<NumberIt;++j)
+  for(int j=0;j<NumberIt;++j)
     {
-#ifdef RICH_MPI
-      N=tess->GetPointNo();
-      res.resize(static_cast<size_t>(N));
-#endif
-      for(int i=0;i<N;++i)
-	  res[static_cast<size_t>(i)] = genNewPoint(*tess, i);
+      res = genNewPoints(*tess);
 #ifdef RICH_MPI
       tproc == 0 ? tess->Update(res) : tess->Update(res,*tproc);
 #else
