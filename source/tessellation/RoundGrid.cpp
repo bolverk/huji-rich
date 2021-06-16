94fca7a176ef30e07c89901d65e50a43bea9dfb4
Bolverk <almog.yalin@gmail.com>
2021-06-16 08:59:47 -0400

    reduce code

--- source/tessellation/RoundGrid.cpp
+++ source/tessellation/RoundGrid.cpp
@@ -1,6 +1,22 @@
 #include "RoundGrid.hpp"
 #include "HilbertOrder.hpp"
 
+namespace {
+  Vector2D genNewPoint
+  (const Tessellation& tess,
+   int i)
+  {
+    const double eta_=0.02,chi_=1;
+    const double R = sqrt(tess.GetVolume(i)/M_PI);
+    const Vector2D s = tess.GetCellCM(i);
+    const Vector2D r = tess.GetMeshPoint(i);
+    const double d = abs(s-r);
+    const Vector2D dw =
+      d/eta_/R<0.95 ? Vector2D(0,0) : chi_*0.5*(s-r);
+    return tess.GetMeshPoint(i) + dw;
+  }
+}
+
 vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
 			   const OuterBoundary& bc,int NumberIt,
 #ifdef RICH_MPI
@@ -15,7 +31,6 @@ vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
 #else
   tess->Initialise(points,bc);
 #endif
-  const double eta_=0.02,chi_=1;
   vector<Vector2D> res
     (static_cast<size_t>(tess->GetPointNo()));
   for(int j=0,N=tess->GetPointNo();j<NumberIt;++j)
@@ -25,15 +40,7 @@ vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
       res.resize(static_cast<size_t>(N));
 #endif
       for(int i=0;i<N;++i)
-	{
-	  const double R = sqrt(tess->GetVolume(i)/M_PI);
-	  const Vector2D s = tess->GetCellCM(i);
-	  const Vector2D r = tess->GetMeshPoint(i);
-	  const double d = abs(s-r);
-	  const Vector2D dw =
-	    d/eta_/R<0.95 ? Vector2D(0,0) : chi_*0.5*(s-r);
-	  res[static_cast<size_t>(i)]=tess->GetMeshPoint(i)+dw;
-	}
+	  res[static_cast<size_t>(i)] = genNewPoint(*tess, i);
 #ifdef RICH_MPI
       tproc == 0 ? tess->Update(res) : tess->Update(res,*tproc);
 #else
@@ -41,9 +48,8 @@ vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
 #endif
     }
 #ifdef RICH_MPI
-  const int N=tess->GetPointNo();
   res=tess->GetMeshPoints();
-  res.resize(static_cast<size_t>(N));
+  res.resize(static_cast<size_t>(tess->GetPointNo()));
 #endif
   return res;
 }
