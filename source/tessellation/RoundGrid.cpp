d209a81ec471a0c1a8f496cda3f4803c6ba3f915
Bolverk <almog.yalin@gmail.com>
2021-06-16 08:36:05 -0400

    simplify code

--- source/tessellation/RoundGrid.cpp
+++ source/tessellation/RoundGrid.cpp
@@ -3,51 +3,47 @@
 
 vector<Vector2D> RoundGrid(vector<Vector2D> const& points,
 			   const OuterBoundary& bc,int NumberIt,
-			   #ifdef RICH_MPI
+#ifdef RICH_MPI
 			   Tessellation const* tproc,
-			   #endif
-	Tessellation *tess)
+#endif
+			   Tessellation *tess)
 {
   VoronoiMesh default_tess;
   tess = tess == nullptr ? &default_tess : tess;
 #ifdef RICH_MPI
   tproc == 0 ? tess->Initialise(points,bc) : tess->Initialise(points,*tproc,bc);
 #else
-	tess->Initialise(points,bc);
+  tess->Initialise(points,bc);
 #endif
-	const double eta_=0.02,chi_=1;
-	vector<Vector2D> res
-	  (static_cast<size_t>(tess->GetPointNo()));
-	for(int j=0,N=tess->GetPointNo();j<NumberIt;++j)
+  const double eta_=0.02,chi_=1;
+  vector<Vector2D> res
+    (static_cast<size_t>(tess->GetPointNo()));
+  for(int j=0,N=tess->GetPointNo();j<NumberIt;++j)
+    {
+#ifdef RICH_MPI
+      N=tess->GetPointNo();
+      res.resize(static_cast<size_t>(N));
+#endif
+      for(int i=0;i<N;++i)
 	{
-#ifdef RICH_MPI
-	  N=tess->GetPointNo();
-		res=tess->GetMeshPoints();
-		res.resize(static_cast<size_t>(N));
-#endif
-		for(int i=0;i<N;++i)
-		{
-			double R = sqrt(tess->GetVolume(i)/M_PI);
-			Vector2D s = tess->GetCellCM(i);
-			Vector2D r = tess->GetMeshPoint(i);
-			double d = abs(s-r);
-			Vector2D dw;
-			if(d/eta_/R<0.95)
-				dw = 0*s;
-			else
-				dw = chi_*0.5*(s-r);
-			res[static_cast<size_t>(i)]=tess->GetMeshPoint(i)+dw;
-		}
-#ifdef RICH_MPI
-		tproc == 0 ? tess->Update(res) : tess->Update(res,*tproc);
-#else
-		tess->Update(res);
-#endif
+	  const double R = sqrt(tess->GetVolume(i)/M_PI);
+	  const Vector2D s = tess->GetCellCM(i);
+	  const Vector2D r = tess->GetMeshPoint(i);
+	  const double d = abs(s-r);
+	  const Vector2D dw =
+	    d/eta_/R<0.95 ? Vector2D(0,0) : chi_*0.5*(s-r);
+	  res[static_cast<size_t>(i)]=tess->GetMeshPoint(i)+dw;
 	}
 #ifdef RICH_MPI
-	const int N=tess->GetPointNo();
-	res=tess->GetMeshPoints();
-	res.resize(static_cast<size_t>(N));
+      tproc == 0 ? tess->Update(res) : tess->Update(res,*tproc);
+#else
+      tess->Update(res);
 #endif
-	return res;
+    }
+#ifdef RICH_MPI
+  const int N=tess->GetPointNo();
+  res=tess->GetMeshPoints();
+  res.resize(static_cast<size_t>(N));
+#endif
+  return res;
 }
